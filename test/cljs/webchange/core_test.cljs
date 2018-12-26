6658b356ecd45d37506a534df76843db91c31fc4
Ivan Khaldeev <ikhaldeev@yandex.ru>
2018-12-26 18:31:57 +0400

    Fix tests for deploy

--- test/cljs/webchange/core_test.cljs
+++ test/cljs/webchange/core_test.cljs
@@ -2,10 +2,6 @@
   (:require [cljs.test :refer-macros [deftest testing is]]
             [webchange.core :as core]))
 
-(deftest fake-test
-  (testing "fake description"
-    (is (= 1 2))))
-
 (deftest fake-test-pass
   (testing "should pass"
     (is (= 2 2))))
