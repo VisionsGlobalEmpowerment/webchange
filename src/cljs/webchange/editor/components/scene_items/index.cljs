f416e3f23666747b76be9427596c8ad3e24f202b
wildeastengineer <wildeastengineer@gmail.com>
2019-05-03 15:03:24 +0400

    Extract action templates block

--- src/cljs/webchange/editor/components/scene_items/index.cljs
+++ src/cljs/webchange/editor/components/scene_items/index.cljs
@@ -9,6 +9,7 @@
     [webchange.editor.action-properties.core :refer [action-types] :as action-properties]
     [webchange.editor.common.components :refer [dispatch-properties-panel
                                                 update-object-action]]
+    [webchange.editor.components.scene-items.action-templates.index :refer [list-action-templates-panel]]
     [webchange.editor.components.scene-items.actions.index :refer [list-actions-panel]]
     [webchange.editor.components.scene-items.animations.index :refer [list-animations-panel]]
     [webchange.editor.components.scene-items.assets.index :refer [list-assets-panel]]
@@ -22,31 +23,6 @@
     [webchange.editor.subs :as es]
     [webchange.subs :as subs]
     ))
-
-(defn list-action-templates-panel
-  []
-  (let [scene-id @(re-frame/subscribe [::subs/current-scene])]
-    (fn []
-      [na/segment {}
-       [na/header {:as "h4" :floated "left" :content "Action templates"}]
-       [:div {:style {:float "right"}}
-        [na/icon {:name "close" :link? true :on-click #(re-frame/dispatch [::events/reset-shown-form])}]]
-       [na/divider {:clearing? true}]
-
-       [sa/ItemGroup {:divided true :style {:overflow "auto" :max-height "500px"}}
-        (for [action-type (map #(-> % :value name) action-types)]
-          ^{:key action-type}
-          [sa/Item {:draggable true :on-drag-start #(-> (.-dataTransfer %) (.setData "text/plain" (-> {:type "action" :id (name action-type)}
-                                                                                                      clj->js
-                                                                                                      js/JSON.stringify)))}
-
-           [sa/ItemContent {}
-            [sa/ItemHeader {:as "a"}
-             (str action-type)]
-
-            [sa/ItemDescription {}
-             ]]]
-          )]])))
 
 ;; Duplicated from webchange.editor.index
 (defn reset-transform
