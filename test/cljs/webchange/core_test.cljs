5129cebb260f2fdfbbd593013734f16d1e83f9e9
Dmitrii Gorlov <wildeastengineer@gmail.com>
2019-05-24 10:56:56 +0400

    Fix tests

--- test/cljs/webchange/core_test.cljs
+++ test/cljs/webchange/core_test.cljs
@@ -1,6 +1,6 @@
 (ns webchange.core-test
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs.test :refer-macros [deftest testing is use-fixtures]]
+  (:require [cljs.test :refer-macros [async deftest testing is use-fixtures]]
             [cljs.core.async :refer [<! timeout]]
             [re-frame.core :as re-frame]
             [day8.re-frame.test :as rf-test]
@@ -17,18 +17,20 @@
 
 (use-fixtures :each {:before (fn [] (re-frame/dispatch-sync [::events/initialize-db]))})
 
-(deftest course-can-be-loaded
-  (rf-test/run-test-async
-    (testing "initial scene is set when course is loaded"
-      (re-frame/dispatch [::ie/start-course "test-course"])
-      (rf-test/wait-for [::ie/set-current-scene]
-                        (is (= "initial-scene" (get-in @re-frame.db/app-db [:current-scene])))))))
+(deftest example-with-timeout
+  (async done
+    (rf-test/run-test-async
+      (testing "initial scene is set when course is loaded"
+        (re-frame/dispatch [::ie/start-course "test-course"])
+        (rf-test/wait-for [::ie/set-current-scene]
+                          (is (= "initial-scene" (get-in @re-frame.db/app-db [:current-scene])))
+                          (done))))))
 
 (deftest scene-can-be-loaded
-  (rf-test/run-test-async
-    (testing "load scene"
-      (re-frame/dispatch [::ie/start-course "test-course"])
-      (rf-test/wait-for [::ie/set-scene]
-        (is (= fixtures/initial-scene (get-in @re-frame.db/app-db [:scenes "initial-scene"])))))))
-
-
+  (async done
+    (rf-test/run-test-async
+      (testing "load scene"
+        (re-frame/dispatch [::ie/start-course "test-course"])
+        (rf-test/wait-for [::ie/set-scene]
+                          (is (= fixtures/initial-scene (get-in @re-frame.db/app-db [:scenes "initial-scene"])))
+                          (done))))))
