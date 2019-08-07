73271cf20e9a9236c90a5f0b5e77234300e0bb80
Ivan Khaldeev <ikhaldeev@yandex.ru>
2019-08-07 13:57:12 +0400

    Allow submit class form with Enter keypress

--- src/cljs/webchange/dashboard/classes/class_modal/views.cljs
+++ src/cljs/webchange/dashboard/classes/class_modal/views.cljs
@@ -6,13 +6,12 @@
     [webchange.dashboard.classes.subs :as dcs]
     [webchange.dashboard.classes.events :as dce]))
 
-(defn- class-form
+(defn- class-form-inputs
   [props]
-  [:form
-   [ui/text-field
-    {:label         "Class Name"
-     :default-value (:name @props)
-     :on-change     #(swap! props assoc :name (->> % .-target .-value))}]])
+  [ui/text-field
+   {:label         "Class Name"
+    :default-value (:name @props)
+    :on-change     #(swap! props assoc :name (->> % .-target .-value))}])
 
 (defn class-modal
   []
@@ -27,24 +26,26 @@
     [ui/dialog
      {:open     (boolean class-modal-state)
       :on-close handle-close}
-     [ui/dialog-title
-      (case class-modal-state
-        :add "Add New Class"
-        :edit "Edit Class"
-        "")]
-     [ui/dialog-content
-      (if (:class loading)
-        [ui/circular-progress
-         {:size  80
-          :color "secondary"}]
-        [class-form class-data])]
-     [ui/dialog-actions
-      [ui/button
-       {:on-click handle-close}
-       "Cancel"]
-      [ui/button
-       {:variant  "contained"
-        :color    "primary"
-        :on-click #(do (handle-save @class-data)
-                       (handle-close))}
-       "Save"]]]))
+     [:form
+      [ui/dialog-title
+       (case class-modal-state
+         :add "Add New Class"
+         :edit "Edit Class"
+         "")]
+      [ui/dialog-content
+       (if (:class loading)
+         [ui/circular-progress
+          {:size  80
+           :color "secondary"}]
+         [class-form-inputs class-data])]
+      [ui/dialog-actions
+       [ui/button
+        {:on-click handle-close}
+        "Cancel"]
+       [ui/button
+        {:type     "submit"
+         :variant  "contained"
+         :color    "primary"
+         :on-click #(do (handle-save @class-data)
+                        (handle-close))}
+        "Save"]]]]))
