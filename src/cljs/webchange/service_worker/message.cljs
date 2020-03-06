7282f389f65e46e129bda7fb4e7008f4e0199156
Dmitrii Gorlov <wildeastengineer@gmail.com>
2020-03-06 15:46:13 +0400

    Fix sw

--- src/cljs/webchange/service_worker/message.cljs
+++ src/cljs/webchange/service_worker/message.cljs
@@ -1,9 +1,15 @@
-(ns webchange.service-worker.message)
+(ns webchange.service-worker.message
+  (:require
+    [webchange.logger :as logger]))
 
 (defn- get-worker
   []
-  (-> (.. js/navigator -serviceWorker -ready)
-      (.then #(.-active %))))
+  (let [service-worker (.-serviceWorker js/navigator)]
+    (if-not (nil? service-worker)
+      (-> (.-ready service-worker)
+          (.then (fn [sw]
+                   (.-active sw))))
+      (js/Promise.reject (js/Error. "Service worker is not defined.")))))
 
 (def message-types {:cache-scenes           "update-cached-scenes"
                     :get-cached-resources   "get-cached-resources"
@@ -13,9 +19,13 @@
 (defn- post-message
   [message]
   (-> (get-worker)
-      (.then #(->> message
-                   (clj->js)
-                   (.postMessage %)))))
+      (.then (fn [sw]
+               (if-not (nil? sw)
+                 (->> message
+                      (clj->js)
+                      (.postMessage sw))
+                 (logger/warn "Can not post message: Service worker is not defined."))))
+      (.catch (fn [e] (logger/warn "Can not post message." e)))))
 
 (defn get-cached-resources
   []
