e34f51895de7b96545822734783e6db0b7453b1c
Ivan Khaldeev <ikhaldeev@yandex.ru>
2020-03-20 12:18:18 +0400

    Fix audios list in translator

--- src/cljs/webchange/editor_v2/translator/views_modal.cljs
+++ src/cljs/webchange/editor_v2/translator/views_modal.cljs
@@ -26,19 +26,19 @@
   []
   (let [actions-data (atom {})
         scene-id (re-frame/subscribe [::subs/current-scene])
-        translator-modal-state @(re-frame/subscribe [::translator-subs/translator-modal-state])
+        open? @(re-frame/subscribe [::translator-subs/translator-modal-state])
         handle-save #(do (save-actions-data! actions-data @scene-id)
                          (close-window!))
         handle-close #(close-window!)]
     [ui/dialog
-     {:open       (boolean translator-modal-state)
+     {:open       open?
       :on-close   handle-close
       :full-width true
       :max-width  "xl"}
      [ui/dialog-title
       "Phrase Translation"]
      [ui/dialog-content {:class-name "translation-form"}
-      (when (boolean translator-modal-state)
+      (when open?
         [translator-form actions-data])]
      [ui/dialog-actions
       [ui/button {:on-click handle-close}
