3ce0afb300e26fc53504b82010271878841ccc52
Dmitrii Gorlov <wildeastengineer@gmail.com>
2020-04-14 10:05:27 +0400

    Fix tranlation blocks

--- src/cljs/webchange/editor_v2/translator/translator_form/views_form_diagram.cljs
+++ src/cljs/webchange/editor_v2/translator/translator_form/views_form_diagram.cljs
@@ -2,8 +2,19 @@
   (:require
     [webchange.editor-v2.diagram.widget :refer [diagram-widget]]))
 
+(defn- update-graph
+  [graph data-store current-concept-id]
+  (reduce (fn [graph [[action-name concept-id] action-data]]
+            (if (or (nil? concept-id)
+                    (= concept-id current-concept-id))
+              (update-in graph [action-name :data] merge (:data action-data))
+              graph))
+          graph
+          data-store))
+
 (defn diagram-block
-  [{:keys [graph]}]
-  [diagram-widget {:graph graph
-                   :mode  :translation}])
+  [{:keys [graph current-concept edited-data]}]
+  (let [prepared-graph (update-graph graph edited-data (:id current-concept))]
+    [diagram-widget {:graph prepared-graph
+                     :mode  :translation}]))
 
