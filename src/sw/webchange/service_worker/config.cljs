ec4ad02bb6d4224798ed304d0f9b4775015572e6
Ivan Khaldeev <ikhaldeev@yandex.ru>
2020-08-25 15:54:30 +0400

    Update build number

--- src/sw/webchange/service_worker/config.cljs
+++ src/sw/webchange/service_worker/config.cljs
@@ -3,7 +3,7 @@
     [clojure.string :refer [join]]))
 
 (def app-name "webchange")
-(def release-number 9)
+(def release-number 10)
 
 (def cache-names-prefix app-name)
 (def database-name app-name)
