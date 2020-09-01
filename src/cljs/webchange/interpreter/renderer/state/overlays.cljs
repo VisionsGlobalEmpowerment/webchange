577863d48d918eb5511ceaa0749ab59321bdd6e0
Dmitrii Gorlov <wildeastengineer@gmail.com>
2020-09-01 09:19:18 +0400

    Fix elements hiding
    - Set visibility instead of opacity
    - Hide navigation menu when other overlay screen is shown

--- src/cljs/webchange/interpreter/renderer/state/overlays.cljs
+++ src/cljs/webchange/interpreter/renderer/state/overlays.cljs
@@ -5,25 +5,38 @@
     [webchange.interpreter.renderer.state.scene :as scene]))
 
 (re-frame/reg-event-fx
-  ::open-settings
+  ::show-navigation-menu
+  (fn [_]
+    {:dispatch [::scene/change-scene-object :navigation-menu [[:set-visibility {:visible true}]]]}))
+
+(re-frame/reg-event-fx
+  ::hide-navigation-menu
+  (fn [_]
+    {:dispatch [::scene/change-scene-object :navigation-menu [[:set-visibility {:visible false}]]]}))
+
+(re-frame/reg-event-fx
+  ::show-settings
   (fn [{:keys [db]}]
     (let [music-volume (get-in db [:settings :music-volume])
           effects-volume (get-in db [:settings :effects-volume])]
-      {:dispatch-n (list [::scene/change-scene-object :settings-overlay [[:set-visibility {:visible true}]]]
+      {:dispatch-n (list [::hide-navigation-menu]
+                         [::scene/change-scene-object :settings-overlay [[:set-visibility {:visible true}]]]
                          [::scene/change-scene-object :music-slider [[:set-value music-volume]]]
                          [::scene/change-scene-object :effects-slider [[:set-value effects-volume]]])})))
 
 (re-frame/reg-event-fx
-  ::close-settings
+  ::hide-settings
   (fn []
-    {:dispatch [::scene/change-scene-object :settings-overlay [[:set-visibility {:visible false}]]]}))
+    {:dispatch-n (list [::show-navigation-menu]
+                       [::scene/change-scene-object :settings-overlay [[:set-visibility {:visible false}]]])}))
 
 (re-frame/reg-event-fx
   ::open-activity-finished
   (fn [{:keys [db]}]
     (let [next-activity (subs/next-activity db)
           lesson-progress (subs/lesson-progress db)]
-      {:dispatch-n (list [::scene/change-scene-object :activity-finished-overlay [[:set-visibility {:visible true}]]]
+      {:dispatch-n (list [::hide-navigation-menu]
+                         [::scene/change-scene-object :activity-finished-overlay [[:set-visibility {:visible true}]]]
                          [::scene/change-scene-object :overall-progress-bar [[:set-value lesson-progress]]]
                          [::scene/change-scene-object :next-activity-card-image [[:set-src (:image next-activity)]]]
                          [::scene/change-scene-object :next-activity-card-name [[:set-text (:name next-activity)]]])})))
@@ -31,7 +44,8 @@
 (re-frame/reg-event-fx
   ::close-activity-finished
   (fn []
-    {:dispatch [::scene/change-scene-object :activity-finished-overlay [[:set-visibility {:visible false}]]]}))
+    {:dispatch-n (list [::show-navigation-menu]
+                       [::scene/change-scene-object :activity-finished-overlay [[:set-visibility {:visible false}]]])}))
 
 (re-frame/reg-event-fx
   ::show-skip-menu
