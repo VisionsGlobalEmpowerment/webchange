0dc4c2301616bfcd3fa7476dad17d1cc59e3794a
Dmitrii Gorlov <wildeastengineer@gmail.com>
2020-09-04 12:54:56 +0400

    Fix transparent component

--- src/cljs/webchange/interpreter/renderer/scene/components/transparent/component.cljs
+++ src/cljs/webchange/interpreter/renderer/scene/components/transparent/component.cljs
@@ -1,6 +1,6 @@
 (ns webchange.interpreter.renderer.scene.components.transparent.component
   (:require
-    [webchange.interpreter.renderer.pixi :refer [Container Rectangle]]
+    [webchange.interpreter.renderer.pixi :refer [Container Sprite Texture]]
     [webchange.interpreter.renderer.scene.components.transparent.wrapper :refer [wrap]]
     [webchange.interpreter.renderer.scene.components.utils :as utils]))
 
@@ -9,21 +9,34 @@
                     :width    {}
                     :height   {}
                     :name     {}
-                    :on-click {}})
+                    :on-click {}
+                    :ref      {}
+                    :scale    {:default {:x 1 :y 1}}})
 
 (defn- create-container
-  [{:keys [x y width height]}]
+  [{:keys [x y scale]}]
   (doto (Container.)
     (utils/set-position {:x x :y y})
-    (utils/set-size {:width width :height height})
-    (set! -hitArea (Rectangle. 0 0 width height))))
+    (utils/set-scale scale)))
+
+(defn- create-sprite
+  [{:keys [width height]}]
+  (doto (Sprite. (.-EMPTY Texture))
+    (utils/set-size {:width  width
+                     :height height})))
 
 (def component-type "transparent")
 
 (defn create
-  [parent {:keys [type object-name on-click] :as props}]
-  (let [container (create-container props)]
-    (when on-click
-      (utils/set-handler container "click" on-click))
+  [parent {:keys [type object-name on-click ref  ] :as props}]
+  (let [container (create-container props)
+        sprite (create-sprite props)
+        wrapped-container (wrap type object-name container)]
+
+    (when on-click (utils/set-handler container "click" on-click))
+    (when ref (ref wrapped-container))
+
+    (.addChild container sprite)
     (.addChild parent container)
-    (wrap type object-name container)))
+
+    wrapped-container))
