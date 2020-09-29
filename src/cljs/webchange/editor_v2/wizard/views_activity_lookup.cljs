21094416d2f3333d2449ba5b2de5f7370091ad11
Dmitrii Gorlov <wildeastengineer@gmail.com>
2020-09-29 17:40:28 +0400

    Validate rest options

--- src/cljs/webchange/editor_v2/wizard/views_activity_lookup.cljs
+++ src/cljs/webchange/editor_v2/wizard/views_activity_lookup.cljs
@@ -1,23 +1,30 @@
 (ns webchange.editor-v2.wizard.views-activity-lookup
   (:require
-    [cljs-react-material-ui.reagent :as ui]))
+    [reagent.core :as r]
+    [cljs-react-material-ui.reagent :as ui]
+    [webchange.editor-v2.wizard.validator :as v :refer [connect-data]]))
+
+(def lookup-validation-map {:root [(fn [value] (when (nil? value) "Required field"))]})
 
 (defn lookup-option
-  [{:keys [key option data]}]
-  [ui/grid {:container   true
-            :justify     "center"
-            :spacing     16
-            :align-items "center"}
-   [ui/grid {:item true :xs 12}
-    [ui/typography {:variant "h6"
-                    :style   {:display      "inline-block"
-                              :margin-right "16px"}}
-     (:label option)]
-    [ui/form-control {:style {:margin-top "-18px"}}
-     [ui/input-label "Value"]
-     [ui/select {:value     (get @data key "")
-                 :on-change #(swap! data assoc key (-> % .-target .-value))
-                 :style     {:min-width "150px"}}
-      (for [{:keys [name value]} (:options option)]
-        ^{:key value}
-        [ui/menu-item {:value value} name])]]]])
+  [{:keys [key option data validator]}]
+  (r/with-let [lookup-data (connect-data data [key])
+               {:keys [error-message]} (v/init lookup-data lookup-validation-map validator)]
+    [ui/grid {:container   true
+              :justify     "center"
+              :spacing     16
+              :align-items "center"}
+     [ui/grid {:item true :xs 12}
+      [ui/typography {:variant "h6"
+                      :style   {:display      "inline-block"
+                                :margin-right "16px"}}
+       (:label option)]
+      [ui/form-control {:style {:margin-top "-18px"}}
+       [ui/input-label "Value"]
+       [ui/select {:value     (or @lookup-data "")
+                   :on-change #(reset! lookup-data (-> % .-target .-value))
+                   :style     {:min-width "150px"}}
+        (for [{:keys [name value]} (:options option)]
+          ^{:key value}
+          [ui/menu-item {:value value} name])]]
+      [error-message {:field-name :root}]]]))
--- src/cljs/webchange/editor_v2/wizard/views_activity_string.cljs
+++ src/cljs/webchange/editor_v2/wizard/views_activity_string.cljs
@@ -1,20 +1,27 @@
 (ns webchange.editor-v2.wizard.views-activity-string
   (:require
-    [cljs-react-material-ui.reagent :as ui]))
+    [reagent.core :as r]
+    [cljs-react-material-ui.reagent :as ui]
+    [webchange.editor-v2.wizard.validator :as v :refer [connect-data]]))
+
+(def string-validation-map {:root [(fn [value] (when (nil? value) "Required field"))]})
 
 (defn string-option
-  [{:keys [key option data]}]
-  [ui/grid {:container   true
-            :justify     "center"
-            :spacing     16
-            :align-items "center"}
-   [ui/grid {:item true :xs 12}
-    [ui/typography {:variant "h6"
-                    :style   {:display      "inline-block"
-                              :margin-right "16px"}}
-     (:label option)]
-    [ui/text-field {:label     "Value"
-                    :variant   "outlined"
-                    :value     (get @data key "")
-                    :style     {:min-width "300px"}
-                    :on-change #(swap! data assoc key (-> % .-target .-value))}]]])
+  [{:keys [key option data validator]}]
+  (r/with-let [string-data (connect-data data [key])
+               {:keys [error-message]} (v/init string-data string-validation-map validator)]
+    [ui/grid {:container   true
+              :justify     "center"
+              :spacing     16
+              :align-items "center"}
+     [ui/grid {:item true :xs 12}
+      [ui/typography {:variant "h6"
+                      :style   {:display      "inline-block"
+                                :margin-right "16px"}}
+       (:label option)]
+      [ui/text-field {:label     "Value"
+                      :variant   "outlined"
+                      :value     (or @string-data "")
+                      :style     {:min-width "300px"}
+                      :on-change #(reset! string-data (-> % .-target .-value))}]
+      [error-message {:field-name :root}]]]))
