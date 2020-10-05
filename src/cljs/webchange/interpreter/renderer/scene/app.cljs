f1137f95908e771aa660f42db31e235891bd9f16
Dmitrii Gorlov <wildeastengineer@gmail.com>
2020-10-05 11:36:05 +0400

    Fix cycling back-out
    Remove all tickers when app reset

--- src/cljs/webchange/interpreter/renderer/scene/app.cljs
+++ src/cljs/webchange/interpreter/renderer/scene/app.cljs
@@ -4,12 +4,34 @@
     [webchange.interpreter.pixi :refer [clear-texture-cache destroy-texture-cache sound]]))
 
 (def pixi-app (atom nil))
+(def ticker-handlers (atom {}))
 
 (defn app-exists? [] (not (nil? @pixi-app)))
+
+(defn add-ticker
+  [f]
+  (swap! ticker-handlers assoc f f)
+  (-> @pixi-app
+      .-ticker
+      (.add f)))
+
+(defn remove-ticker
+  [f]
+  (swap! ticker-handlers dissoc f)
+  (-> @pixi-app
+      .-ticker
+      (.remove f)))
+
+(defn- remove-all-tickers
+  []
+  (doall (for [[ticker] @ticker-handlers]
+           (remove-ticker ticker)))
+  (reset! ticker-handlers {}))
 
 (defn reset-app!
   []
   (when (app-exists?)
+    (remove-all-tickers)
     (let [stage (.-stage @pixi-app)
           children (.removeChildren stage)]
       (doall (for [child children]
@@ -29,18 +51,6 @@
 (defn register-app
   [app]
   (reset! pixi-app app))
-
-(defn add-ticker
-  [f]
-  (-> @pixi-app
-      .-ticker
-      (.add f)))
-
-(defn remove-ticker
-  [f]
-  (-> @pixi-app
-      .-ticker
-      (.remove f)))
 
 (defn get-renderer
   []
