a53754caf5e03da4a7170858cf2531f87743ede8
Dmitry Gorlov <wildeastengineer@gmail.com>
2020-10-07 19:07:28 +0400

    Fix viewport (#352)

--- src/cljs/webchange/interpreter/renderer/stage_utils.cljs
+++ src/cljs/webchange/interpreter/renderer/stage_utils.cljs
@@ -1,30 +1,32 @@
 (ns webchange.interpreter.renderer.stage-utils)
 
 (defn- compute-shift
-  [display-size scale key]
-  (let [dimension (get display-size key)]
-    (-> (- scale 1)
-        (* dimension)
-        (/ 2))))
+  [viewport target-size scale key]
+  (let [viewport-dimension (get viewport key)
+        target-dimension (get target-size key)]
+    (->> (* scale target-dimension)
+         (- viewport-dimension)
+         (* 0.5))))
 
 (defn- compute-scale
-  [viewport {:keys [display-width display-height]}]
-  (let [original-ratio (/ display-width display-height)
+  [viewport target-size]
+  (let [original-ratio (/ (:width target-size) (:height target-size))
         window-ratio (/ (:width viewport) (:height viewport))]
     (if (> original-ratio window-ratio)
-      (/ (:height viewport) display-height)
-      (/ (:width viewport) display-width))))
+      (/ (:height viewport) (:height target-size))
+      (/ (:width viewport) (:width target-size)))))
 
 (defn get-stage-params
   [viewport]
   (when (some? viewport)
-    (let [display-size {:display-width  1920
-                        :display-height 1080}
-          scale (compute-scale viewport display-size)]
-      (merge display-size
-             {:x       (-> (compute-shift display-size scale :width) (Math/round))
-              :y       (-> (compute-shift display-size scale :height) (Math/round))
-              :width   (:width viewport)
-              :height  (:height viewport)
-              :scale-x scale
-              :scale-y scale}))))
+    (let [target-size {:width  1920
+                       :height 1080}
+          scale (compute-scale viewport target-size)]
+      {:x             (-> (compute-shift viewport target-size scale :width) (Math/round))
+       :y             (-> (compute-shift viewport target-size scale :height) (Math/round))
+       :width         (:width viewport)
+       :height        (:height viewport)
+       :target-width  (:width target-size)
+       :target-height (:height target-size)
+       :scale-x       scale
+       :scale-y       scale})))
