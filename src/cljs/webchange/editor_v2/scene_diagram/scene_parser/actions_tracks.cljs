f61add336d16f16a5792baf667ad73b7e848dad1
Dmitrii Gorlov <wildeastengineer@gmail.com>
2020-11-03 11:09:08 +0400

    Support tracks metadata

--- src/cljs/webchange/editor_v2/scene_diagram/scene_parser/actions_tracks.cljs
+++ src/cljs/webchange/editor_v2/scene_diagram/scene_parser/actions_tracks.cljs
@@ -3,12 +3,26 @@
 (def default-track "Etc")
 
 (defn get-actions-tracks
-  [scene-data dialog-actions-paths]
-  (let [actions-data (map (fn [action-path]
-                            [action-path (get-in scene-data (concat [:actions] action-path))])
-                          dialog-actions-paths)]
-    (reduce (fn [result [action-path action-data]]
-              (let [track (:dialog-track action-data)]
-                (update-in result [(or track default-track)] conj action-path)))
-            {}
-            actions-data)))
+  [scene-data dialog-actions-names]
+  (let [metadata-tracks (get-in scene-data [:metadata :tracks] [])
+        metadata-tracks-actions (->> metadata-tracks
+                                     (map :nodes)
+                                     (flatten)
+                                     (filter (fn [{:keys [type]}] (= type "dialog")))
+                                     (map :action-id)
+                                     (map keyword))
+        actions-data (->> dialog-actions-names
+                          (filter (fn [action-name] (not (some #{action-name} metadata-tracks-actions))))
+                          (map (fn [action-name]
+                                 [action-name (get-in scene-data [:actions action-name])])))]
+    (->> actions-data
+         (reduce (fn [result [action-path action-data]]
+                   (let [track (:dialog-track action-data)]
+                     (update-in result [(or track default-track)] conj action-path)))
+                 {})
+         (reduce (fn [result [track-name track-actions]]
+                   (conj result {:title track-name
+                                 :nodes (map (fn [action-id] {:type      "dialog"
+                                                              :action-id action-id})
+                                             track-actions)}))
+                 metadata-tracks))))
