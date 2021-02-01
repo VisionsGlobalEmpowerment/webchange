31faa4ac76ad0846bfde66c9701c3124c8982ead
Dmitrii Gorlov <wildeastengineer@gmail.com>
2021-02-01 14:32:45 +0400

    Api request error in warehouse

--- src/cljs/webchange/warehouse.cljs
+++ src/cljs/webchange/warehouse.cljs
@@ -13,38 +13,47 @@
           form-params))
 
 (defn- create-request
-  [{:keys [method uri body params]} {:keys [on-success on-failure]}]
+  [{:keys [key method uri body params]} {:keys [on-success on-failure]}]
   {:http-xhrio (cond-> {:method          method
                         :uri             uri
                         :format          (json-request-format)
                         :response-format (json-response-format {:keywords? true})
-                        :on-success      [::empty-success-handler]}
+                        :on-success      [::empty-success-handler]
+                        :on-failure      [::generic-failure-handler key on-failure]}
                        (some? params) (assoc :params params)
                        (some? body) (assoc :body body)
-                       (some? on-success) (assoc :on-success on-success)
-                       (some? on-failure) (assoc :on-failure on-failure))})
+                       (some? on-success) (assoc :on-success on-success))})
 
 (re-frame/reg-event-fx
   ::empty-success-handler
   (fn [] {}))
 
 (re-frame/reg-event-fx
+  ::generic-failure-handler
+  (fn [{:keys [_]} [_ key failure-handler response]]
+    {:dispatch-n (cond-> [[:api-request-error key response]]
+                         (some? failure-handler) (conj (conj failure-handler response)))}))
+
+(re-frame/reg-event-fx
   ::load-skills
   (fn [{:keys [_]} [_ handlers]]
-    (create-request {:method :get
+    (create-request {:key    :load-skills
+                     :method :get
                      :uri    (str "/api/skills")} handlers)))
 
 (re-frame/reg-event-fx
   ::update-scene-skills
   (fn [{:keys [_]} [_ {:keys [course-id scene-id skills-ids]} handlers]]
-    (create-request {:method :post
+    (create-request {:key    :update-scene-skills
+                     :method :post
                      :uri    (str "/api/courses/" course-id "/scenes/" scene-id "/skills")
                      :params {:skills skills-ids}} handlers)))
 
 (re-frame/reg-event-fx
   ::load-course
   (fn [{:keys [_]} [_ course-slug handlers]]
-    (create-request {:method :get
+    (create-request {:key    :load-course
+                     :method :get
                      :uri    (str "/api/courses/" course-slug)}
                     {:on-success [::load-course-success handlers]
                      :on-failure (:on-failure handlers)})))
@@ -58,7 +67,8 @@
 (re-frame/reg-event-fx
   ::save-course
   (fn [{:keys [_]} [_ {:keys [course-slug course-data]} handlers]]
-    (create-request {:method :post
+    (create-request {:key    :save-course
+                     :method :post
                      :uri    (str "/api/courses/" course-slug)
                      :params {:course course-data}} handlers)))
 
@@ -67,7 +77,8 @@
 (re-frame/reg-event-fx
   ::create-lesson-set
   (fn [{:keys [_]} [_ {:keys [dataset-id name data]} handlers]]
-    (create-request {:method :post
+    (create-request {:key    :create-lesson-set
+                     :method :post
                      :uri    (str "/api/lesson-sets")
                      :params {:dataset-id dataset-id
                               :name       name
@@ -84,14 +95,16 @@
 (re-frame/reg-event-fx
   ::update-lesson-set
   (fn [{:keys [_]} [_ {:keys [id data]} handlers]]
-    (create-request {:method :put
+    (create-request {:key    :update-lesson-set
+                     :method :put
                      :uri    (str "/api/lesson-sets/" id)
                      :params {:data data}} handlers)))
 
 (re-frame/reg-event-fx
   ::delete-lesson-set
   (fn [{:keys [_]} [_ {:keys [id]} handlers]]
-    (create-request {:method :delete
+    (create-request {:key    :delete-lesson-set
+                     :method :delete
                      :uri    (str "/api/lesson-sets/" id)}
                     {:on-success [::delete-lesson-set-success handlers]
                      :on-failure (:on-failure handlers)})))
@@ -105,7 +118,8 @@
 (re-frame/reg-event-fx
   ::create-activity-placeholder
   (fn [_ [_ {:keys [course-id data]} handlers]]
-    (create-request {:method :post
+    (create-request {:key    :create-activity-placeholder
+                     :method :post
                      :uri    (str "/api/courses/" course-id "/create-activity-placeholder")
                      :params data} handlers)))
 
@@ -116,7 +130,8 @@
   (fn [{:keys [db]} [_ {:keys [file form-params]
                         :or   {form-params []}} handlers]]
     (let [form-data (get-form-data (concat [["file" file]] form-params))]
-      (create-request {:method :post
+      (create-request {:key    :upload-audio
+                       :method :post
                        :uri    (str "/api/assets/")
                        :body   form-data} handlers))))
 
