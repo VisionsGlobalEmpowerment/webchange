22ad99ba2c9b612bdadcb8858833d63c17727188
Dmitrii Gorlov <wildeastengineer@gmail.com>
2021-02-12 16:25:53 +0400

    Fix text input

--- src/cljs/webchange/editor_v2/layout/flipbook/page_text/views_text_control.cljs
+++ src/cljs/webchange/editor_v2/layout/flipbook/page_text/views_text_control.cljs
@@ -2,21 +2,55 @@
   (:require
     [cljs-react-material-ui.reagent :as ui]
     [re-frame.core :as re-frame]
+    [reagent.core :as r]
     [webchange.editor-v2.layout.flipbook.page-text.state :as state]))
+
+(defn- update-rows-number
+  [el value]
+  (->> value
+       (filter #(re-find #"\n" %))
+       (count)
+       (inc)
+       (set! (.-rows el))))
+
+(defn- text-input
+  []
+  (let [el (atom nil)
+        dirty? (atom false)]
+    (r/create-class
+      {:display-name "stage-text-input"
+
+       :component-did-update
+                     (fn [this]
+                       (let [{:keys [value]} (r/props this)]
+                         (when (and (not (= value (.-value @el)))
+                                    (not @dirty?))
+                           (set! (.-value @el) value)
+                           (update-rows-number @el value))))
+
+       :reagent-render
+                     (fn [{:keys [value on-change disabled?]}]
+                       [ui/text-field {:default-value value
+                                       :on-change     (fn [event]
+                                                        (let [value (.. event -target -value)]
+                                                          (reset! dirty? true)
+                                                          (update-rows-number @el value)
+                                                          (on-change value)))
+                                       :placeholder   "Enter page text here"
+                                       :multiline     true
+                                       :full-width    true
+                                       :variant       "outlined"
+                                       :disabled      disabled?
+                                       :inputProps    {:ref   #(when (some? %) (reset! el %))
+                                                       :style {:height   "auto"
+                                                               :overflow "hidden"}}}])})))
 
 (defn text-control
   [{:keys [id]}]
   (let [value @(re-frame/subscribe [::state/current-text id])
-        handle-change (fn [event]
-                        (let [text (.. event -target -value)]
-                          (re-frame/dispatch [::state/set-current-text id text])))
+        handle-change (fn [value] (re-frame/dispatch [::state/set-current-text id value]))
         loading? @(re-frame/subscribe [::state/loading? id])]
     [:div
-     [ui/text-field {:value       value
-                     :on-change   handle-change
-                     :placeholder "Enter page text here"
-                     :multiline   true
-                     :full-width  true
-                     :variant     "outlined"
-                     :disabled    loading?
-                     :inputProps  {:style {:overflow "hidden"}}}]]))
+     [text-input {:value     value
+                  :on-change handle-change
+                  :disabled? loading?}]]))
