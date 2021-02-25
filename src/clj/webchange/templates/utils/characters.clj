76b84b77e8fc16b7d747dba7de003633a5dfe1c2
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-02-25 23:22:38 +0400

    Fix characters with uppercase letters

--- src/clj/webchange/templates/utils/characters.clj
+++ src/clj/webchange/templates/utils/characters.clj
@@ -1,4 +1,5 @@
-(ns webchange.templates.utils.characters)
+(ns webchange.templates.utils.characters
+  (:require [clojure.string :refer [lower-case]]))
 
 (defn create-character
   [character animations]
@@ -11,9 +12,9 @@
   [t characters character-positions animations]
   (let [cs (->> characters
                 (map-indexed (fn [idx c] (merge c (get character-positions idx))))
-                (map (fn [c] [(-> c :name keyword) (create-character c animations)]))
+                (map (fn [c] [(-> c :name lower-case keyword) (create-character c animations)]))
                 (into {}))
-        names (->> cs keys (map name) (into []))]
+        names (->> cs keys (map name) (map lower-case) (into []))]
     (-> t
         (update :objects merge cs)
         (update :scene-objects conj names))))
