8cbeaa8c0ca212612d84e2c9bd0fd4673418af16
Irmatov Pavel <pavel.irmatov@opensoftdev.ru>
2021-03-10 16:12:44 +0400

    Weaken restrictions (#472)

---
+++ resources/migrations/20210309101455-weaken-restrictions.up.sql
@@ -0,0 +1,6 @@
+alter table lesson_sets alter column name TYPE varchar(150);
+--;;
+alter table courses alter column name TYPE varchar(150);
+--;;
+alter table scenes alter column name TYPE varchar(150);
+--;;
