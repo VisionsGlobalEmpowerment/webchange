a035eb76ddd2f4ff3af984d8c00d16279c7982e3
Dmitrii Gorlov <wildeastengineer@gmail.com>
2021-05-03 12:40:10 +0400

    Preview slides

--- src/cljs/webchange/game_changer/templates_list/views.cljs
+++ src/cljs/webchange/game_changer/templates_list/views.cljs
@@ -3,20 +3,29 @@
     [re-frame.core :as re-frame]
     [reagent.core :as r]
     [webchange.game-changer.templates-list.state :as state]
+    [webchange.game-changer.templates-list.views-preview :refer [activity-preview]]
     [webchange.ui-framework.components.utils :refer [get-class-name]]))
 
 (defn- template-list-item
   [{:keys [template on-click selected?]}]
-  (let [{:keys [description name preview]} template]
-    [:div {:class-name (get-class-name {"templates-list-item" true
-                                        "selected"            selected?})
-           :on-click   #(on-click template)}
-     [:div {:class-name (get-class-name {"preview"     true
-                                         "placeholder" (not (some? preview))})
-            :style      (cond-> {}
-                                (some? preview) (assoc :background-image (str "url(" preview ")")))}]
-     [:span.title name]
-     [:div.description description]]))
+  (r/with-let [slideshow-visible? (r/atom false)
+               handle-mouse-enter (fn [] (reset! slideshow-visible? true))
+               handle-mouse-leave (fn [] (reset! slideshow-visible? false))]
+    (let [{:keys [description name preview preview-anim]} template]
+      [:div {:class-name     (get-class-name {"templates-list-item" true
+                                              "selected"            selected?})
+             :on-click       #(on-click template)
+             :on-mouse-enter handle-mouse-enter
+             :on-mouse-leave handle-mouse-leave}
+       [:div {:class-name (get-class-name {"preview"     true
+                                           "placeholder" (not (some? preview))})
+              :style      (cond-> {}
+                                  (some? preview) (assoc :background-image (str "url(" preview ")")))}
+        (when (and (some? preview-anim)
+                   @slideshow-visible?)
+          [activity-preview {:slides preview-anim}])]
+       [:span.title name]
+       [:div.description description]])))
 
 (defn templates-list
   [{:keys [data]}]
