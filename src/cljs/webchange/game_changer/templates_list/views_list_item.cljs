8029e93fdb1568d04db97b0ecf216d4360d2f354
Dmitrii Gorlov <wildeastengineer@gmail.com>
2021-05-04 15:04:11 +0400

    Preview template

--- src/cljs/webchange/game_changer/templates_list/views_list_item.cljs
+++ src/cljs/webchange/game_changer/templates_list/views_list_item.cljs
@@ -2,14 +2,25 @@
   (:require
     [reagent.core :as r]
     [webchange.game-changer.templates-list.views-preview :refer [activity-preview]]
+    [webchange.editor-v2.sandbox.create-link :refer [create-link]]
+    [webchange.ui-framework.components.index :refer [button]]
     [webchange.ui-framework.components.utils :refer [get-class-name]]))
+
+(defn- preview-button
+  [{:keys [activity-slug course-slug]}]
+  (let [link (create-link {:course-slug course-slug
+                           :scene-slug  activity-slug})]
+    [button {:href       link
+             :class-name "preview-button"}
+     "Preview"]))
 
 (defn template-list-item
   [{:keys [template on-click selected?]}]
   (r/with-let [slideshow-visible? (r/atom false)
                handle-mouse-enter (fn [] (reset! slideshow-visible? true))
                handle-mouse-leave (fn [] (reset! slideshow-visible? false))]
-    (let [{:keys [description name preview preview-anim]} template]
+    (let [{:keys [description name props] :or {props {}}} template
+          {:keys [preview preview-activity preview-anim]} props]
       [:div.templates-list-item-place-holder
        [:div {:class-name     (get-class-name {"templates-list-item" true
                                                "selected"            selected?})
@@ -24,6 +35,7 @@
                     @slideshow-visible?)
            [activity-preview {:slides preview-anim}])]
         [:div.title name]
-        [:div.description description]
-
-        ]])))
+        [:div.description
+         description
+         (when (some? preview-activity)
+           [preview-button preview-activity])]]])))
