a8ab8c87ff28a48cf6ab2c8930fb66c2665daec8
Dmitrii Gorlov <wildeastengineer@gmail.com>
2021-05-06 07:20:46 +0400

    Fix dropdown menu

--- src/cljs/webchange/ui_framework/components/icon_button/index.cljs
+++ src/cljs/webchange/ui_framework/components/icon_button/index.cljs
@@ -5,23 +5,25 @@
     [webchange.ui-framework.components.utils :refer [get-class-name]]))
 
 (defn component
-  [{:keys [class-name color icon disabled? on-click size variant]
+  [{:keys [class-name color icon disabled? on-click ref size variant]
     :or   {disabled? false
            color     "default"
            on-click  #()
            size      "default"
            variant   "default"}}]
   (let [this (r/current-component)
-        children (r/children this)]
-    [:button {:class-name (get-class-name (-> {"wc-icon-button" true
-                                               "with-text" (some? children)}
-                                              (assoc class-name (some? class-name))
-                                              (assoc color true)
-                                              (assoc icon true)
-                                              (assoc size true)
-                                              (assoc variant true)))
-              :disabled   disabled?
-              :on-click   on-click}
+        children (r/children this)
+        handle-ref (fn [el] (when (some? el) (ref el)))]
+    [:button (cond-> {:class-name (get-class-name (-> {"wc-icon-button" true
+                                                       "with-text"      (some? children)}
+                                                      (assoc class-name (some? class-name))
+                                                      (assoc color true)
+                                                      (assoc icon true)
+                                                      (assoc size true)
+                                                      (assoc variant true)))
+                      :disabled   disabled?
+                      :on-click   on-click}
+                     (fn? ref) (assoc :ref handle-ref))
      [icon/component {:icon icon}]
      (when (some? children)
        (into [:span]
