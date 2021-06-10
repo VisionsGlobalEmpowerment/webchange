d728c60c160562c5c869888de0e88249b0c0efbd
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-06-11 01:11:43 +0400

    fix upload audio

--- src/cljs/webchange/ui/utils.cljs
+++ src/cljs/webchange/ui/utils.cljs
@@ -4,6 +4,7 @@
   (letfn [(rec-merge [v1 v2]
             (if (and (map? v1) (map? v2))
               (merge-with deep-merge v1 v2)
-              v2))]
-    (when (some identity vs)
-      (reduce #(rec-merge %1 %2) v vs))))
+              (or v2 v1)))]
+    (if (some identity vs)
+      (reduce #(rec-merge %1 %2) v vs)
+      v)))
