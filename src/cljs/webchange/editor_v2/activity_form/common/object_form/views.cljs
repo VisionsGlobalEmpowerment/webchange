a10ec49cd0bd5a22f7eb3c49aff84df1d565da62
Dmitrii Gorlov <wildeastengineer@gmail.com>
2021-06-17 13:25:51 +0400

    Removed group fix

--- src/cljs/webchange/editor_v2/activity_form/common/object_form/views.cljs
+++ src/cljs/webchange/editor_v2/activity_form/common/object_form/views.cljs
@@ -25,25 +25,31 @@
 (defn- object-form-view
   [{:keys [class-name objects-data objects-names object-type on-destroy on-save-click]
     :or   {on-save-click #()}}]
-  (r/with-let [id (->> (random-uuid) (str) (take 8) (clojure.string/join ""))]
-    (let [component (get form-components object-type)
-          component-props {:id            id
-                           :objects-data  objects-data
-                           :objects-names objects-names}
-          disabled? @(re-frame/subscribe [::state/disabled? id])]
-      [:div {:class-name (get-class-name (-> {"activity-object-form" true}
-                                             (assoc class-name (some? class-name))))}
-       [:div.object-form-content
-        [component component-props]]
-       [:div.buttons-block
-        [button {:variant   "contained"
-                 :color     "primary"
-                 :size      "big"
-                 :disabled? disabled?
-                 :on-click  #(on-save-click id)}
-         "Apply"]]])
-    (finally
-      (on-destroy id))))
+  (if (available-object-type? object-type)
+    (r/with-let [id (->> (random-uuid) (str) (take 8) (clojure.string/join ""))]
+      (let [component (get form-components object-type)
+            component-props {:id            id
+                             :objects-data  objects-data
+                             :objects-names objects-names}
+            disabled? @(re-frame/subscribe [::state/disabled? id])]
+        [:div {:class-name (get-class-name (-> {"activity-object-form" true}
+                                               (assoc class-name (some? class-name))))}
+         [:div.object-form-content
+          [component component-props]]
+         [:div.buttons-block
+          [button {:variant   "contained"
+                   :color     "primary"
+                   :size      "big"
+                   :disabled? disabled?
+                   :on-click  #(on-save-click id)}
+           "Apply"]]])
+      (finally
+        (on-destroy id)))
+    (do (logger/warn "Form not defined")
+        (logger/warn "type" object-type)
+        (logger/warn "names" objects-names)
+        (logger/warn "data" objects-data)
+        nil)))
 
 (defn- group-form
   [{:keys [component-key objects-data] :as props}]
@@ -55,22 +61,24 @@
                              (let [object-data (utils/get-scene-object scene-data child-name)]
                                {:object-type   (get object-data :type)
                                 :objects-data  object-data
-                                :objects-names [child-name]}))))
-          current-asset (nth assets @current-asset-idx)
-          component-key (->> (get current-asset :objects-names)
-                             (map clojure.core/name)
-                             (clojure.string/join "--")
-                             (str component-key "--"))]
+                                :objects-names [child-name]})))
+                      (filter (fn [{:keys [object-type]}]
+                                (available-object-type? object-type))))
+          current-asset (nth assets @current-asset-idx nil)]
       [:div.group-form
        [:ul.menu
-        (for [[idx {:keys [objects-names]}] (map-indexed vector assets)]
-          ^{:key idx}
-          [:li {:class-name (get-class-name {"menu-item" true
-                                             "selected"  (= idx @current-asset-idx)})
-                :on-click   #(reset! current-asset-idx idx)}
-           (first objects-names)])]
-       ^{:key component-key}
-       [object-form-view (merge props current-asset)]])))
+        (doall (for [[idx {:keys [objects-names]}] (map-indexed vector assets)]
+                 ^{:key idx}
+                 [:li {:class-name (get-class-name {"menu-item" true
+                                                    "selected"  (= idx @current-asset-idx)})
+                       :on-click   #(reset! current-asset-idx idx)}
+                  (first objects-names)]))]
+       (when (some? current-asset)
+         ^{:key (->> (get current-asset :objects-names)
+                     (map clojure.core/name)
+                     (clojure.string/join "--")
+                     (str component-key "--"))}
+         [object-form-view (merge props current-asset)])])))
 
 (defn object-form
   []
@@ -92,12 +100,9 @@
                            :objects-names names
                            :on-save-click handle-save
                            :on-destroy    handle-destroy}]
-      (cond
-        (and (some? data) (= type "group")) ^{:key component-key} [group-form (merge component-props
-                                                                                     {:component-key component-key})]
-        (and (some? data) (available-object-type? type)) ^{:key component-key} [object-form-view component-props]
-        :else (do (logger/warn "Form not defined")
-                  (logger/warn "type" type)
-                  (logger/warn "names" names)
-                  (logger/warn "data" data)
-                  nil)))))
+      (when (some? data)
+        (if (= type "group")
+          ^{:key component-key}
+          [group-form (merge component-props {:component-key component-key})]
+          ^{:key component-key}
+          [object-form-view component-props])))))
