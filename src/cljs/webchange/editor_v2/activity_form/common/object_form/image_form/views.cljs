429e92ae4cecf1531c2ad061e2cd55fcaedeb90d
Dmitrii Gorlov <wildeastengineer@gmail.com>
2021-06-17 13:54:01 +0400

    Image preview

--- src/cljs/webchange/editor_v2/activity_form/common/object_form/image_form/views.cljs
+++ src/cljs/webchange/editor_v2/activity_form/common/object_form/image_form/views.cljs
@@ -4,24 +4,31 @@
     [reagent.core :as r]
     [webchange.editor-v2.activity-form.common.object-form.image-form.state :as state]
     [webchange.common.image-selector.views-modal :refer [with-image-modal]]
-    [webchange.ui-framework.components.index :refer [button file label]]))
+    [webchange.ui-framework.components.index :refer [button file label]]
+    [webchange.ui-framework.components.utils :refer [get-class-name]]))
 
 (defn form
   [{:keys [class-name id objects-data objects-names]}]
   (r/with-let [_ (re-frame/dispatch [::state/init id objects-data objects-names])]
-    (let [image-tag @(re-frame/subscribe [::state/image-tag id])
+    (let [image-src @(re-frame/subscribe [::state/image-src id])
+          image-tag @(re-frame/subscribe [::state/image-tag id])
           handle-change (fn [src]
                           (re-frame/dispatch [::state/set-image-src id src]))]
-      [:div (cond-> {} (some? class-name) (assoc :class-name class-name))
-       [:div.control-block
-        [label "Pick image from library:"]
-        [with-image-modal {:tag       image-tag
-                           :on-change handle-change}
-         [button {:variant "contained"
-                  :color   "primary"}
-          "Select Image"]]]
-       [:div.control-block
-        [label "Or upload your own image:"]
-        [file {:type       "image"
-               :show-icon? false
-               :on-change  handle-change}]]])))
+      [:div {:class-name (get-class-name (cond-> {"image-form" true}
+                                                 (some? class-name) (assoc :class-name true)))}
+       [:div.preview
+        (when (some? image-src)
+          [:img {:src image-src}])]
+       [:div.controls
+        [:div.control-block
+         [label "Pick image from library:"]
+         [with-image-modal {:tag       image-tag
+                            :on-change handle-change}
+          [button {:variant "contained"
+                   :color   "primary"}
+           "Select Image"]]]
+        [:div.control-block
+         [label "Or upload your own image:"]
+         [file {:type       "image"
+                :show-icon? false
+                :on-change  handle-change}]]]])))
