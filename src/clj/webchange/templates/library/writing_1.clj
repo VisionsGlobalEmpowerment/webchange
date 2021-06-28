52978f70a5d240c498222dcdae4694dc72bd6273
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-06-28 22:10:00 +0400

    fix writing activities

--- src/clj/webchange/templates/library/writing_1.clj
+++ src/clj/webchange/templates/library/writing_1.clj
@@ -14,49 +14,46 @@
                    {:url "/raw/img/library/painting-tablet/brush.png", :size 10, :type "image"}
                    {:url "/raw/img/library/painting-tablet/felt-tip.png", :size 10, :type "image"}
                    {:url "/raw/img/library/painting-tablet/pencil.png", :size 10, :type "image"}
-                   {:url "/raw/img/library/painting-tablet/eraser.png", :size 10, :type "image"}],
+                   {:url "/raw/img/library/painting-tablet/eraser.png", :size 10, :type "image"}
+                   {:url "/raw/img/ui/checkmark.png" :size 1 :type "image"}],
         :objects
-                  {:background {:type "background", :scene-name "background", :src "/raw/img/library/painting-tablet/background.jpg"},
+                  {:background  {:type "background", :scene-name "background", :src "/raw/img/library/painting-tablet/background.jpg"},
 
                    :practice-canvas
-                               {:type    "painting-area"
-                                :tool "felt-tip"
-                                :color "#4479bb"
-                                :change  {:on "click" :type "action" :id "timeout-timer"}}
+                                {:type    "painting-area"
+                                 :tool    "felt-tip"
+                                 :color   "#4479bb"
+                                 :actions {:change {:on "click" :type "action" :id "timeout-timer"}}}
                    :painting-toolset
-                               {:type       "painting-toolset"
-                                :transition "painting-toolset"
-                                :actions    {:change {:on "change" :type "action" :id "set-current-tool" :pick-event-param "tool"}}}
+                                {:type       "painting-toolset"
+                                 :transition "painting-toolset"
+                                 :actions    {:change {:on "change" :type "action" :id "set-current-tool" :pick-event-param "tool"}}}
                    :colors-palette
-                               {:type       "colors-palette",
-                                :transition "colors-palette"
-                                :actions    {:change {:on "change" :type "action", :id "set-current-color" :pick-event-param "color"}}}
+                                {:type       "colors-palette",
+                                 :transition "colors-palette"
+                                 :actions    {:change {:on "change" :type "action", :id "set-current-color" :pick-event-param "color"}}}
                    :mari
-                               {:type       "animation",
-                                :x          1600,
-                                :y          225,
-                                :width      473,
-                                :height     511,
-                                :scene-name "mari",
-                                :transition "mari",
-                                :anim       "idle",
-                                :name       "mari",
-                                :scale-x    0.5,
-                                :scale-y    0.5,
-                                :speed      0.35,
-                                :start      true
-                                :actions    {:click {:on "click" :type "action" :id "dialog-tap-instructions"}}},
-                   :next-button
-                               {:type     "image",
-                                :x        1800,
-                                :y        915,
-                                :width    97,
-                                :height   99,
-                                :actions  {:click {:id "finish-activity", :on "click", :type "action"}},
-                                :rotation 0,
-                                :scale-x  -1,
-                                :scale-y  1,
-                                :src      "/raw/img/ui/back_button_01.png"}},
+                                {:type       "animation",
+                                 :x          1600,
+                                 :y          225,
+                                 :width      473,
+                                 :height     511,
+                                 :scene-name "mari",
+                                 :transition "mari",
+                                 :anim       "idle",
+                                 :name       "mari",
+                                 :scale-x    0.5,
+                                 :scale-y    0.5,
+                                 :speed      0.35,
+                                 :start      true
+                                 :actions    {:click {:on "click" :type "action" :id "tap-instructions"}}},
+
+                   :next-button {:type    "image"
+                                 :x       1776 :y 48
+                                 :actions {:click {:id "finish-activity", :on "click", :type "action"}}
+                                 :filters [{:name "brightness" :value 0}
+                                           {:name "glow" :outer-strength 0 :color 0xffd700}]
+                                 :src     "/raw/img/ui/checkmark.png"}},
         :scene-objects
                   [["background"]
                    ["practice-canvas"
@@ -75,9 +72,10 @@
                                                         {:type "stop-activity"}]}
                    :finish-activity             {:type "sequence-data"
                                                  :data [{:type "remove-interval" :id "instructions-timeout"}
+                                                        {:type "action" :id "finish-activity-dialog"}
                                                         {:type "finish-activity"}]},
-                   :dialog-instructions         (-> (dialog/default "Instructions")
-                                                    (assoc :available-activities ["show-example"]))
+                   :dialog-instructions         (-> (dialog/default "Instructions"))
+                   :finish-activity-dialog      (-> (dialog/default "Finish"))
 
                    :highlight-tools             {:type               "transition"
                                                  :transition-id      "painting-toolset"
@@ -94,9 +92,8 @@
                    :highlight-next              {:type               "transition"
                                                  :transition-id      "next-button"
                                                  :return-immediately true
-                                                 :from               {:brightness 0},
-                                                 :to                 {:brightness 0.35 :yoyo true :duration 0.5}
-                                                 :kill-after         3000}
+                                                 :from               {:brightness 0 :glow 0}
+                                                 :to                 {:brightness 0.1 :glow 10 :yoyo true :duration 0.5 :repeat 5}}
                    :set-current-tool            {:type "sequence-data"
                                                  :data [{:type        "set-attribute",
                                                          :target      "practice-canvas"
@@ -124,41 +121,52 @@
                    :timeout-timer               {:type     "set-interval",
                                                  :id       "instructions-timeout",
                                                  :action   "dialog-timeout-instructions",
-                                                 :interval 15000}
+                                                 :interval 25000}
 
+                   :tap-instructions {:type "sequence-data"
+                                      :data [{:type "remove-flows" :flow-tag "instructions"}
+                                             {:type "action" :id "dialog-tap-instructions"}]}
                    :dialog-timeout-instructions (-> (dialog/default "Timeout instructions")
-                                                    (assoc :available-activities ["show-example"]))
+                                                    (assoc :tags ["instructions"]))
                    :dialog-tap-instructions     (-> (dialog/default "Tap instructions")
-                                                    (assoc :available-activities ["show-example"]))},
+                                                    (assoc :tags ["instructions"]))},
         :triggers {:stop {:on "back", :action "stop-activity"}, :start {:on "start", :action "start-scene"}},
-        :metadata {:prev   "library", :autostart true
-                   :tracks [{:title "1 Instructions stage 1"
-                             :nodes [{:type      "dialog"
-                                      :action-id :dialog-instructions}
-                                     {:type      "dialog"
-                                      :action-id :dialog-timeout-instructions}
-                                     {:type      "dialog"
-                                      :action-id :dialog-tap-instructions}]}
-                            {:title "2 Colors"
-                             :nodes [{:type      "dialog"
-                                      :action-id :dialog-color-4487611}
-                                     {:type      "dialog"
-                                      :action-id :dialog-color-9616714}
-                                     {:type      "dialog"
-                                      :action-id :dialog-color-15569322}
-                                     {:type      "dialog"
-                                      :action-id :dialog-color-16631089}
-                                     {:type      "dialog"
-                                      :action-id :dialog-color-65793}]}
-                            {:title "3 Tools"
-                             :nodes [{:type      "dialog"
-                                      :action-id :dialog-tool-brush}
-                                     {:type      "dialog"
-                                      :action-id :dialog-tool-felt-tip}
-                                     {:type      "dialog"
-                                      :action-id :dialog-tool-pencil}
-                                     {:type      "dialog"
-                                      :action-id :dialog-tool-eraser}]}]}})
+        :metadata {:prev              "library", :autostart true
+                   :tracks            [{:title "1 Instructions"
+                                        :nodes [{:type      "dialog"
+                                                 :action-id :dialog-instructions}
+                                                {:type      "dialog"
+                                                 :action-id :dialog-timeout-instructions}
+                                                {:type      "dialog"
+                                                 :action-id :dialog-tap-instructions}
+                                                {:type      "dialog"
+                                                 :action-id :finish-activity-dialog}]}
+                                       {:title "2 Colors"
+                                        :nodes [{:type      "dialog"
+                                                 :action-id :dialog-color-4487611}
+                                                {:type      "dialog"
+                                                 :action-id :dialog-color-9616714}
+                                                {:type      "dialog"
+                                                 :action-id :dialog-color-15569322}
+                                                {:type      "dialog"
+                                                 :action-id :dialog-color-16631089}
+                                                {:type      "dialog"
+                                                 :action-id :dialog-color-65793}]}
+                                       {:title "3 Tools"
+                                        :nodes [{:type      "dialog"
+                                                 :action-id :dialog-tool-brush}
+                                                {:type      "dialog"
+                                                 :action-id :dialog-tool-felt-tip}
+                                                {:type      "dialog"
+                                                 :action-id :dialog-tool-pencil}
+                                                {:type      "dialog"
+                                                 :action-id :dialog-tool-eraser}]}]
+                   :available-actions [{:action "highlight-tools"
+                                        :name   "Highlight tools"}
+                                       {:action "highlight-colors"
+                                        :name   "Highlight colors"}
+                                       {:action "highlight-next"
+                                        :name   "Highlight next"}]}})
 
 (defn f
   [t args]
