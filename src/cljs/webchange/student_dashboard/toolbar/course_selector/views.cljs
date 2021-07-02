887c7af9f74bcdcbb3a356fa8f55991fc7ee033a
siddharth <siddharthmishra413@gmail.com>
2021-07-02 15:34:04 +0530

    Removed course-selector menu & added course-slug

--- src/cljs/webchange/student_dashboard/toolbar/course_selector/views.cljs
+++ src/cljs/webchange/student_dashboard/toolbar/course_selector/views.cljs
@@ -1,54 +1,18 @@
-(ns webchange.student-dashboard.toolbar.course-selector.views
+(ns webchange.student-dashboard.toolbar.course.views
   (:require
-    [cljs-react-material-ui.reagent :as ui]
-    [re-frame.core :as re-frame]
-    [webchange.interpreter.events :as ie]
-    [webchange.subs :as subs]))
-
-;; ToDo: get courses list from API
-(def courses
-  [{:key "en" :value "english" :text "English"}
-   {:key "es" :value "spanish" :text "Español"}])
+   [re-frame.core :as re-frame]
+   [webchange.subs :as subs]))
 
 (defn- get-styles
   []
-  {:main {:width  "150px"
-          :margin "0 20px"}
-   :icon {:background-size     "cover"
-          :background-repeat   "no-repeat"
-          :background-position "center"
-          :border-radius       "3px"
-          :display             "inline-block"
-          :height              "14px"
-          :margin-right        "16px"
-          :position            "relative"
-          :top                 "2px"
-          :width               "18px"}})
+  {:main {:width          "150px"
+          :margin         "0 20px"
+          :color          "black"
+          :text-transform "capitalize"
+          :font-family    "Roboto, Helvetica, Arial, sans-serif"}})
 
-(defn- flag
-  [name]
-  (let [src (case name
-              "en" "/icons/flags/us.svg"
-              "es" "/icons/flags/es.svg")
-        styles (get-styles)]
-    [:div {:style (merge (:icon styles)
-                         {:background-image (str "url(" src ")")})}]))
-
-(defn- menu-item
-  [{:keys [key value text]}]
-  [ui/menu-item {:key value :value value}
-   [flag key]
-   [ui/typography {:inline  true
-                   :variant "inherit"}
-    text]])
-
-(defn course-selector
+(defn course
   []
   (let [current-course @(re-frame/subscribe [::subs/current-course])
         styles (get-styles)]
-    [ui/select {:value     current-course
-                :variant   "outlined"
-                :on-change #(re-frame/dispatch [::ie/open-student-course-dashboard (-> % .-target .-value)])
-                :style     (:main styles)}
-     (for [course courses]
-       (menu-item course))]))
+    [:h3 {:style (:main styles)} current-course]))
