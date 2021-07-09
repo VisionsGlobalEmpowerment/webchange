a3e40b8518fe1672787f99e863f503e35c4749ca
siddharth <siddharthmishra413@gmail.com>
2021-07-09 20:49:38 +0530

    added logic for templates seggregation

--- src/cljs/webchange/game_changer/steps/select_template/views_list.cljs
+++ src/cljs/webchange/game_changer/steps/select_template/views_list.cljs
@@ -1,9 +1,10 @@
 (ns webchange.game-changer.steps.select_template.views-list
   (:require
-    [re-frame.core :as re-frame]
-    [reagent.core :as r]
-    [webchange.game-changer.steps.select_template.state :as state]
-    [webchange.game-changer.steps.select_template.views-list-item :refer [template-list-item]]))
+   [re-frame.core :as re-frame]
+   [reagent.core :as r]
+   [webchange.game-changer.steps.select_template.state :as state]
+   [cljs-react-material-ui.reagent :as ui]
+   [webchange.game-changer.steps.select_template.views-list-item :refer [template-list-item]]))
 
 (defn templates-list
   [{:keys [data]}]
@@ -12,9 +13,18 @@
                               (->> (select-keys template [:id :name :options])
                                    (swap! data assoc :template)))]
     (let [templates @(re-frame/subscribe [::state/templates-list])
+          tutorial-activity @(re-frame/subscribe [::state/tutorial-activity])
+          filtered-templates @(re-frame/subscribe [::state/templates-list-without-tutorial-activity])
           current-template-id (get-in @data [:template :id])]
       [:div.templates-list
-       (for [{:keys [id] :as template} templates]
+       [ui/typography {:variant "h5"}
+        "Start with this..."]
+       [template-list-item {:template  tutorial-activity
+                            :selected? (= (:id tutorial-activity) current-template-id)
+                            :on-click  handle-click}]
+       [ui/typography {:variant "h5"}
+        "Choose from other activities..."]
+       (for [{:keys [id] :as template} filtered-templates]
          ^{:key id}
          [template-list-item {:template  template
                               :selected? (= id current-template-id)
