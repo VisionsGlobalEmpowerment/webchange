904c0182180f621b1409720f9231d6f3d52ca066
Dmitrii Gorlov <wildeastengineer@gmail.com>
2021-08-11 16:57:01 +0400

    Scale controls

--- src/cljs/webchange/editor_v2/activity_form/common/object_form/animation_form/views.cljs
+++ src/cljs/webchange/editor_v2/activity_form/common/object_form/animation_form/views.cljs
@@ -4,7 +4,7 @@
     [reagent.core :as r]
     [webchange.editor-v2.activity-form.common.object-form.animation-form.state :as state]
     [webchange.editor-v2.activity-form.common.object-form.animation-form.views-skin :refer [skin]]
-    [webchange.ui-framework.components.index :refer [icon-button label select-image]]))
+    [webchange.ui-framework.components.index :refer [icon-button input label select-image]]))
 
 (defn- flip
   [{:keys [id]}]
@@ -27,10 +27,37 @@
                     :with-arrow? false
                     :show-image? false}]]))
 
+(defn- scale-control
+  [{:keys [on-change title value]}]
+  (let [handle-change (fn [new-value]
+                        (when-not (= new-value 0)
+                          (on-change new-value)))]
+    [:div.scale-group
+     [label title]
+     [input {:value      value
+             :on-change  handle-change
+             :type       "float"
+             :step       0.1
+             :class-name "scale-input"}]]))
+
+(defn- scale
+  [{:keys [id]}]
+  (let [current-scale @(re-frame/subscribe [::state/current-scale id])
+        handle-change (fn [scale-key new-value]
+                        (re-frame/dispatch [::state/set-scale id scale-key new-value]))]
+    [:div.scale-control
+     [scale-control {:value     (:x current-scale)
+                     :on-change #(handle-change :x %)
+                     :title     "Scale X:"}]
+     [scale-control {:value     (:y current-scale)
+                     :on-change #(handle-change :y %)
+                     :title     "Scale Y:"}]]))
+
 (defn form
   [{:keys [id objects-data objects-names]}]
   (r/with-let [_ (re-frame/dispatch [::state/init id objects-data objects-names])]
-    [:div
-     [flip {:id id}]
+    [:div.animation-form
      [skeleton {:id id}]
-     [skin {:id id}]]))
+     [skin {:id id}]
+     [flip {:id id}]
+     [scale {:id id}]]))
