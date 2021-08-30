0042af15bacb406f86cc582ffec637fd03cd3520
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-08-31 00:47:54 +0400

    Implement review table for games

--- src/cljs/webchange/dashboard/courses/subs.cljs
+++ src/cljs/webchange/dashboard/courses/subs.cljs
@@ -3,11 +3,18 @@
     [re-frame.core :as re-frame]))
 
 (re-frame/reg-sub
+  ::books-list
+  (fn [db]
+    (get-in db [:dashboard :courses :books])))
+
+(re-frame/reg-sub
   ::courses-list
   (fn [db]
-    (get-in db [:dashboard :courses])))
+    (get-in db [:dashboard :courses :courses])))
 
 (re-frame/reg-sub
   ::courses-loading
   (fn [db]
-    (get-in db [:loading :courses])))
+    (or
+     (get-in db [:loading :courses])
+     (get-in db [:loading :books]))))
