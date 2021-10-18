d8db9d9b38daf8965ef18195900c92206073a04a
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-10-18 20:56:39 +0400

    increase limits for courses lists

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -88,7 +88,7 @@ SELECT c.*,t.updated_at from courses c
 JOIN (SELECT course_id, max(created_at) as updated_at from course_versions group by course_id) t on(c.id=t.course_id)
 WHERE status = 'published'
 ORDER BY updated_at DESC, name DESC
-LIMIT 30;
+LIMIT 130;
 
 -- :name get-courses-by-status-and-type :? :*
 -- :doc retrieve all available courses
@@ -96,7 +96,7 @@ SELECT c.*, t.updated_at from courses c
 JOIN (SELECT course_id, max(created_at) as updated_at from course_versions group by course_id) t on(c.id=t.course_id)
 WHERE status = :status AND type = :type
 ORDER BY updated_at DESC, name DESC
-LIMIT 30;
+LIMIT 130;
 
 -- :name get-courses-by-website-user :? :*
 -- :doc retrieve draft courses given website user id
@@ -104,7 +104,7 @@ SELECT c.*,t.updated_at from courses c
 JOIN (SELECT course_id, max(created_at) as updated_at from course_versions group by course_id) t on(c.id=t.course_id)
 WHERE status != 'archived' AND type = :type AND website_user_id = :website_user_id
 ORDER BY updated_at DESC, name DESC
-LIMIT 50;
+LIMIT 250;
 
 -- :name get-course-version :? :1
 -- :doc retrieve course version by id
