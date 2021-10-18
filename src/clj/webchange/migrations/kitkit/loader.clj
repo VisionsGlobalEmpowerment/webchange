22e900f09cc211c5edb5bcec6755956e0c98ed4a
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-10-18 20:21:22 +0400

    Import kitkit books

--- src/clj/webchange/migrations/kitkit/loader.clj
+++ src/clj/webchange/migrations/kitkit/loader.clj
@@ -13,26 +13,18 @@
 (defn load-book
   ([config import-dir owner-id]
    (doseq [dir (get-book-name import-dir)]
-     (load-book config import-dir owner-id (.getName dir))
-   ))
+     (load-book config import-dir owner-id (.getName dir))))
   ([config import-dir owner-id name]
    (let [owner-id (Integer. owner-id)
          source-dir  (str import-dir name "/")
          public-dir  (str "/raw/book/" name "/")
          target-dir  (f/relative->absolute-path public-dir)]
-     (-> source-dir
-         (book-import/read-book-info)
-         (book-import/import-assets-book-info source-dir target-dir public-dir)
-         (book-import/prepare-book-info)
-         (book-import/import-book-info owner-id name))))
-  )
-
+     (book-import/import-book owner-id source-dir public-dir target-dir))))
 
 (def commands
   {"load-book"
    (fn [config args]
-     (apply load-book config args))
-   })
+     (apply load-book config args))})
 
 (defn command? [[arg]]
   (contains? (set (keys commands)) arg))
