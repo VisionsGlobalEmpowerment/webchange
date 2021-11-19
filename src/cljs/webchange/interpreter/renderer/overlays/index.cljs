fd09475985c79384332325a8333a058ade1e8760
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-11-19 15:00:04 +0400

    And close button

--- src/cljs/webchange/interpreter/renderer/overlays/index.cljs
+++ src/cljs/webchange/interpreter/renderer/overlays/index.cljs
@@ -3,6 +3,7 @@
     [webchange.interpreter.renderer.overlays.activity-finished :as activity-finished]
     [webchange.interpreter.renderer.overlays.goodbye :as goodbye]
     [webchange.interpreter.renderer.overlays.navigation :as navigation]
+    [webchange.interpreter.renderer.overlays.navigation-back :as navigation-back]
     [webchange.interpreter.renderer.overlays.settings :as settings]
     [webchange.interpreter.renderer.overlays.skip-menu :as skip-menu]
     [webchange.interpreter.renderer.scene.components.create-component :refer [create-component]]))
@@ -15,6 +16,7 @@
                      :children    (cond-> []
                                           (skip-menu/show-overlay? mode) (conj (skip-menu/create {:viewport viewport}))
                                           (navigation/show-overlay? mode) (conj (navigation/create {:viewport viewport}))
+                                          (navigation-back/show-overlay? mode) (conj (navigation-back/create {:viewport viewport}))
                                           (activity-finished/show-overlay? mode) (conj (activity-finished/create {:viewport viewport}))
                                           (goodbye/show-overlay? mode) (conj (goodbye/create {:viewport viewport}))
                                           (settings/show-overlay? mode) (conj (settings/create {:viewport viewport})))}))
@@ -23,6 +25,7 @@
   [viewport]
   (skip-menu/update-viewport {:viewport viewport})
   (navigation/update-viewport {:viewport viewport})
+  (navigation-back/update-viewport {:viewport viewport})
   (activity-finished/update-viewport {:viewport viewport})
   (goodbye/update-viewport {:viewport viewport})
   (settings/update-viewport {:viewport viewport}))
