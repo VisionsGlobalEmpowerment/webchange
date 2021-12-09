567955ce5794e507ed9304db1d78523adc2c9dd5
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-12-09 15:34:35 +0400

    Fix pause video by timer (#771)

--- src/cljs/webchange/interpreter/utils/video.cljs
+++ src/cljs/webchange/interpreter/utils/video.cljs
@@ -1,9 +1,5 @@
 (ns webchange.interpreter.utils.video)
 
-(defn- get-current-time
-  [video]
-  (.-currentTime video))
-
 (defn set-current-time
   [video frame]
   (set! (.-currentTime video) frame))
@@ -21,33 +17,14 @@
   (pause video)
   (set-current-time video 0))
 
-(defn- add-listener
-  [video event handler]
-  (.addEventListener video event handler))
-
-(defn- remove-listener
-  [video event handler]
-  (.removeEventListener video event handler))
-
-(defn- add-time-update-listener
-  [video handler]
-  (add-listener video "timeupdate" handler))
-
-(defn- remove-time-update-listener
-  [video handler]
-  (remove-listener video "timeupdate" handler))
-
 (defn play-range
   ([video from to]
    (play-range video from to #()))
   ([video from to on-end]
-   (let [handler (atom nil)
-         time-update-handler (fn []
-                               (when (> (get-current-time video) to)
-                                 (pause video)
-                                 (remove-time-update-listener video @handler)
-                                 (on-end)))]
-     (reset! handler time-update-handler)
-     (set-current-time video from)
-     (add-time-update-listener video time-update-handler)
-     (play video))))
+   (set-current-time video from)
+   (let [duration (-> (- to from) (* 1000))
+         schedule-pause (fn [] (js/setTimeout #(do (pause video)
+                                                   (on-end))
+                                              duration))]
+     (-> (play video)
+         (.then schedule-pause)))))
