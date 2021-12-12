8bcca33c8ab1a2a7ab5038de9ccc284f65b87d1e
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-12-12 16:55:29 +0400

    Enable Chrome in tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,13 @@
 module.exports = function (config) {
     config.set({
-        browsers: ['FirefoxHeadless'],
+	browsers: ['ChromeHeadless'],
+	//browsers: ['FirefoxHeadless'],
         // The directory where the output file lives
         basePath: 'target',
         // The file itself
         files: ['ci.js'],
         frameworks: ['cljs-test'],
-        plugins: ['karma-cljs-test', 'karma-firefox-launcher'],
+        plugins: ['karma-cljs-test', 'karma-chrome-launcher', 'karma-firefox-launcher'],
         colors: true,
         logLevel: config.LOG_INFO,
         client: {
