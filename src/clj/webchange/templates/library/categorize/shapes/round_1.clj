17c1675f08fd72692cd13ea2a73440aff8715d17
Dmitrii Gorlov <wildeastengineer@gmail.com>
2021-12-15 15:07:33 +0400

    Fix shapes round 1

--- src/clj/webchange/templates/library/categorize/shapes/round_1.clj
+++ src/clj/webchange/templates/library/categorize/shapes/round_1.clj
@@ -7,53 +7,31 @@
 (def var-action-names round-1-template/var-action-names)
 (def all-vars-in-actions round-1-template/all-vars-in-actions)
 
+(defn- word->box
+  [{:keys [word x y]}]
+  {:name     word
+   :position {:x x :y y}
+   :src      (str "/raw/img/categorize-shapes/" word "-box.png")})
+
+(defn- word->item
+  [{:keys [word x y]}]
+  {:target      word
+   :position    {:x x :y y}
+   :src         (str "/raw/img/categorize-shapes/" word ".png")
+   :pick-dialog {:name   (str word "-item")
+                 :phrase word}})
+
 (def template (round-1-template/get-template
-                {:boxes      [{:name     "circle"
-                               :position {:x 1572 :y 777}
-                               :src      "/raw/img/categorize-shapes/circle-box.png"}
-                              {:name     "oval"
-                               :position {:x 69 :y 777}
-                               :src      "/raw/img/categorize-shapes/oval-box.png"}
-                              {:name     "rectangle"
-                               :position {:x 369 :y 777}
-                               :src      "/raw/img/categorize-shapes/rectangle-box.png"}
-                              {:name     "square"
-                               :position {:x 670 :y 777}
-                               :src      "/raw/img/categorize-shapes/square-box.png"}
-                              {:name     "star"
-                               :position {:x 971 :y 777}
-                               :src      "/raw/img/categorize-shapes/star-box.png"}
-                              {:name     "triangle"
-                               :position {:x 1271 :y 777}
-                               :src      "/raw/img/categorize-shapes/triangle-box.png"}]
-                 :items      [{:target      "circle"
-                               :position    {:x 710 :y 442}
-                               :src         "/raw/img/categorize-shapes/circle.png"
-                               :pick-dialog {:name   "oval-item"
-                                             :phrase "oval"}}
-                              {:target      "oval"
-                               :position    {:x 925 :y 124}
-                               :src         "/raw/img/categorize-shapes/oval.png"
-                               :pick-dialog {:name   "oval-item"
-                                             :phrase "oval"}}
-                              {:target      "rectangle"
-                               :position    {:x 617 :y 146}
-                               :src         "/raw/img/categorize-shapes/rectangle.png"
-                               :pick-dialog {:name   "rectangle-item"
-                                             :phrase "rectangle"}}
-                              {:target      "square"
-                               :position    {:x 982 :y 360}
-                               :src         "/raw/img/categorize-shapes/square.png"
-                               :pick-dialog {:name   "square-item"
-                                             :phrase "square"}}
-                              {:target      "star"
-                               :position    {:x 1214 :y 289}
-                               :src         "/raw/img/categorize-shapes/star.png"
-                               :pick-dialog {:name   "star-item"
-                                             :phrase "star"}}
-                              {:target      "triangle"
-                               :position    {:x 424 :y 377}
-                               :src         "/raw/img/categorize-shapes/triangle.png"
-                               :pick-dialog {:name   "triangle-item"
-                                             :phrase "triangle"}}]
+                {:boxes      [(word->box {:word "circle" :x 1572 :y 777})
+                              (word->box {:word "oval" :x 69 :y 777})
+                              (word->box {:word "rectangle" :x 369 :y 777})
+                              (word->box {:word "square" :x 670 :y 777})
+                              (word->box {:word "star" :x 971 :y 777})
+                              (word->box {:word "triangle" :x 1271 :y 777})]
+                 :items      [(word->item {:word "circle" :x 710 :y 442})
+                              (word->item {:word "oval" :x 925 :y 124})
+                              (word->item {:word "rectangle" :x 617 :y 146})
+                              (word->item {:word "square" :x 982 :y 360})
+                              (word->item {:word "star" :x 1214 :y 289})
+                              (word->item {:word "triangle" :x 424 :y 377})]
                  :background {:src "/raw/img/categorize-shapes/background-white.png"}}))
