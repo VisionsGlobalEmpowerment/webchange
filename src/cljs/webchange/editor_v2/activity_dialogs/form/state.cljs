3aa9dab0a28705c2887487a19bcd9a741d91238f
Dmitrii Gorlov <wildeastengineer@gmail.com>
2022-01-10 13:03:00 +0400

    Fix add phrase action

--- src/cljs/webchange/editor_v2/activity_dialogs/form/state.cljs
+++ src/cljs/webchange/editor_v2/activity_dialogs/form/state.cljs
@@ -346,6 +346,7 @@
   (fn [{:keys [_]} [_ {:keys [action] :as data}]]
     (case action
       "add-effect-action" {:dispatch [::add-effect-action data]}
+      "add-phrase-action" {:dispatch [::add-phrase-action data]}
       "set-target-animation" {:dispatch [::set-target-animation data]}
       "remove-target-animation" {:dispatch [::remove-target-animation data]}
       "start-skip-region" {:dispatch [::start-skip-region data]}
@@ -370,6 +371,15 @@
           action-data (defaults/get-effect-action-data {:action-name id})]
       {:dispatch [::state-dialog-form/insert-action (merge {:action-data action-data}
                                                            position-data)]})))
+
+(re-frame/reg-event-fx
+  ::add-phrase-action
+  (fn [{:keys [_]} [_ {:keys [destination] :as data}]]
+    (let [position-data (get-action-position-data data)
+          action-data defaults/default-action]
+      (case destination
+        "scene" {:dispatch [::state-dialog-form/insert-action (merge {:action-data action-data}
+                                                                     position-data)]}))))
 
 (re-frame/reg-event-fx
   ::set-target-animation
--- src/cljs/webchange/editor_v2/activity_dialogs/menu/sections/phrase/views.cljs
+++ src/cljs/webchange/editor_v2/activity_dialogs/menu/sections/phrase/views.cljs
@@ -1,11 +1,11 @@
 (ns webchange.editor-v2.activity-dialogs.menu.sections.phrase.views
   (:require
-   [re-frame.core :as re-frame]
-   [webchange.editor-v2.activity-dialogs.menu.sections.common.section-block.views :refer [section-block]]
-   [webchange.editor-v2.activity-dialogs.menu.sections.common.options-list.views :refer [options-list]]
-   [webchange.editor-v2.activity-dialogs.menu.sections.phrase.state :as state]
-   [webchange.editor-v2.activity-dialogs.form.state :as form-state]
-   [webchange.ui-framework.components.index :refer [icon-button label select]]))
+    [re-frame.core :as re-frame]
+    [webchange.editor-v2.activity-dialogs.menu.sections.common.section-block.views :refer [section-block]]
+    [webchange.editor-v2.activity-dialogs.menu.sections.common.options-list.views :refer [options-list]]
+    [webchange.editor-v2.activity-dialogs.menu.sections.phrase.state :as state]
+    [webchange.editor-v2.activity-dialogs.form.state :as form-state]
+    [webchange.ui-framework.components.index :refer [icon-button label select]]))
 
 (defn- concepts-control
   []
@@ -20,38 +20,20 @@
               :type      "int"
               :variant   "outlined"}]]))
 
-(defn- destination
-  []
-  (let [available-destinations @(re-frame/subscribe [::state/available-destinations])
-        handle-click #(re-frame/dispatch [::state/set-current-destination %])]
-    [:div
-     [:span.input-label "Select destination:"]
-     [options-list {:options  available-destinations
-                    :on-click handle-click}]]))
-
 (defn- actions
   []
-  (let [available-actions (->> @(re-frame/subscribe [::state/available-actions])
-                               (map (fn [action]
-                                      (get {:before   ["Before" "insert-before" #(re-frame/dispatch [::state/add-phrase-action :before])]
-                                            :after    ["After" "insert-after" #(re-frame/dispatch [::state/add-phrase-action :after])]
-                                            :parallel ["Parallel" "insert-parallel" #(re-frame/dispatch [::state/add-phrase-action :parallel])]}
-                                           action))))]
+  (let [available-actions @(re-frame/subscribe [::state/available-actions])]
     [:div.actions
-     [:span.input-label "Add:"]
-     (for [[idx [text icon handler]] (map-indexed vector available-actions)]
-       ^{:key idx}
-       [icon-button
-        {:icon     icon
-         :size     "small"
-         :on-click handler}
-        text])]))
+     [options-list {:options       available-actions
+                    :get-drag-data (fn [{:keys [value]}]
+                                     {:action      "add-phrase-action"
+                                      :destination value})}]]))
 
 (defn form
   []
-  [:div.phrase-form
-   [section-block {:title "Concepts"}
-    [concepts-control]]
-   [section-block {:title "Add phrase"}
-    [destination]
-    [actions]]])
+  (let [show-concepts? @(re-frame/subscribe [::form-state/show-concepts?])]
+    [:div.phrase-form
+     (when show-concepts?
+       [section-block {:title "Concepts"}
+        [concepts-control]])
+     [actions]]))
