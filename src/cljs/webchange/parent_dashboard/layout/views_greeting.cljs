7fde6aaa724117848c699cc37890aea27c969012
Dmitrii Gorlov <wildeastengineer@gmail.com>
2022-01-25 10:40:41 +0400

    Greeting message

--- src/cljs/webchange/parent_dashboard/layout/views_greeting.cljs
+++ src/cljs/webchange/parent_dashboard/layout/views_greeting.cljs
@@ -4,8 +4,11 @@
   []
   [:div.greeting
    [:div.message
-    [:h1 "Welcome to TabSchool!"]
-    [:p
-     "Thank you for joining us in our mission to provide quality educational resources for all students!
-    We are excited to have Beta versions of our Pre-K English literacy games available for your student(s) to play!"]]
-   [:img {:src "/images/parent_dashboard/greeting.png"}]])
+    [:h1 "Thank You for Beta Testing our Games!"]
+    [:p "Please help us improve our PreK literacy content by using the link below to send comments,
+    bug reports, and suggestions."]
+    [:a {:href       "https://www.google.com/"
+         :target     "_blank"
+         :rel        "noopener noreferrer"
+         :class-name "button"}
+     "Give feedback"]]])
