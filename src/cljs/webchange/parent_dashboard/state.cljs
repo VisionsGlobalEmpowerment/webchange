6296eba6fb39493b0c90d4f738f0d2380f7e0c78
Dmitrii Gorlov <wildeastengineer@gmail.com>
2022-01-26 15:39:01 +0400

    Parent page wrapper

--- src/cljs/webchange/parent_dashboard/state.cljs
+++ src/cljs/webchange/parent_dashboard/state.cljs
@@ -1,7 +1,40 @@
-(ns webchange.parent-dashboard.state)
+(ns webchange.parent-dashboard.state
+  (:require
+    [re-frame.core :as re-frame]
+    [webchange.state.warehouse :as warehouse]))
 
 (defn path-to-db
   [relative-path]
   (->> relative-path
        (concat [:parent-dashboard])
        (vec)))
+
+(re-frame/reg-event-fx
+  ::init
+  (fn [{:keys [_]} [_]]
+    {:dispatch [::warehouse/login-as-student-parent {}
+                {:on-success [::update-auth-state {:ready? true}]}]}))
+
+(re-frame/reg-event-fx
+  ::reset
+  (fn [{:keys [_]} [_]]
+    {:dispatch [::update-auth-state {:ready? false}]}))
+
+(def auth-state-path (path-to-db [:auth-state]))
+
+(re-frame/reg-sub
+  ::auth-state
+  (fn [db]
+    (get-in db auth-state-path {:ready? false})))
+
+(re-frame/reg-event-fx
+  ::update-auth-state
+  (fn [{:keys [db]} [_ data]]
+    {:db (update-in db auth-state-path merge data)}))
+
+(re-frame/reg-sub
+  ::ready?
+  (fn []
+    (re-frame/subscribe [::auth-state]))
+  (fn [state]
+    (get state :ready?)))
--- src/cljs/webchange/parent_dashboard/views.cljs
+++ src/cljs/webchange/parent_dashboard/views.cljs
@@ -1,20 +1,39 @@
 (ns webchange.parent-dashboard.views
   (:require
+    [re-frame.core :as re-frame]
+    [reagent.core :as r]
     [webchange.parent-dashboard.add-student.views :as add-student]
     [webchange.parent-dashboard.help.views :as help]
-    [webchange.parent-dashboard.students-list.views :as students-list]))
+    [webchange.parent-dashboard.state :as state]
+    [webchange.parent-dashboard.students-list.views :as students-list]
+    [webchange.parent-dashboard.ui.index :refer [circular-progress]]))
+
+(defn parent-page
+  []
+  (r/create-class
+    {:display-name           "parent-page"
+     :component-did-mount    (fn []
+                               (re-frame/dispatch [::state/init]))
+     :component-will-unmount (fn []
+                               (re-frame/dispatch [::state/reset]))
+     :reagent-render         (fn []
+                               (let [ready? @(re-frame/subscribe [::state/ready?])]
+                                 (into [:div.parent-page]
+                                       (if ready?
+                                         (-> (r/current-component) (r/children))
+                                         [[circular-progress {:class-name "parent-page-loader"}]]))))}))
 
 (defn dashboard
   []
-  [:div.parent-page
+  [parent-page
    [students-list/students-list-page]])
 
 (defn add-student
   []
-  [:div.parent-page
+  [parent-page
    [add-student/add-student-page]])
 
 (defn help
   []
-  [:div.parent-page
+  [parent-page
    [help/help-page]])
