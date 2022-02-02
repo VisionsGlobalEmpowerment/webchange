ac36cfc26fde59417e34785ebdc1e2831c5e4fc6
Dmitrii Gorlov <wildeastengineer@gmail.com>
2022-02-02 11:19:07 +0400

    Fix tests

--- src/cljs/webchange/common/svg_path/path_to_transitions.cljs
+++ src/cljs/webchange/common/svg_path/path_to_transitions.cljs
@@ -1,11 +1,11 @@
 (ns webchange.common.svg-path.path-to-transitions
   (:require
-    [clojure.edn :as edn]
     [clojure.string :as s]
     [webchange.common.svg-path.path-splitter :refer [split-path apply-path-to-point]]
     [webchange.common.svg-path.path-element :refer [length]]
     [webchange.interpreter.renderer.scene.components.letters-path :refer [get-svg-path]]
-    [svg-arc-to-cubic-bezier :as arcToBezier]))
+    [svg-arc-to-cubic-bezier :as arcToBezier]
+    [webchange.utils.numbers :refer [to-precision]]))
 
 (defn- apply-origin
   [{:keys [x y]} [path-type & coordinates]]
@@ -52,72 +52,86 @@
     path))
 
 (defn- get-transition
-  [{:keys [x y]} prev-path [path-type & coordinates]]
-  (case path-type
-    ("M" "L")
-    (let [[x1 y1] coordinates]
-      [{:x x1
-        :y y1}])
-    ("H")
-    (let [[x1] coordinates]
-      [{:x x1
-        :y y}])
-    ("V")
-    (let [[y1] coordinates]
-      [{:x x
-        :y y1}])
-    ("C")
-    (let [[x1 y1 x2 y2 x3 y3] coordinates]
-      [{:bezier [{:x x1 :y y1} {:x x2 :y y2} {:x x3 :y y3}]}])
-    ("S")
-    (let [[x2 y2 x3 y3] coordinates
-          prev-path-type (first prev-path)]
-      (if (contains? #{"C" "S"} prev-path-type)
-        (let [[x01 y01 x02 y02] (take-last 4 prev-path)
-              x1 (- (* 2 x02) x01)
-              y1 (- (* 2 y02) y01)]
-          [{:bezier [{:x x1 :y y1} {:x x2 :y y2} {:x x3 :y y3}]}])
-        [{:bezier [{:x x2 :y y2} {:x x2 :y y2} {:x x3 :y y3}]}]))
-    ("Q")
-    (let [[x1 y1 x2 y2] coordinates]
-      [{:bezier [{:x x1 :y y1} {:x x2 :y y2}]}])
-    ("T")
-    (let [[x2 y2] coordinates
-          prev-path-type (first prev-path)]
-      (if (contains? #{"Q"} prev-path-type)
-        (let [[x01 y01 x02 y02] (take-last 4 prev-path)
-              x1 (- (* 2 x02) x01)
-              y1 (- (* 2 y02) y01)]
-          [{:bezier [{:x x1 :y y1} {:x x2 :y y2}]}])
-        [{:x x2 :y y2}]))
-    ("A")
-    (let [[rx ry x-axis-rotation large-arc-flag sweep-flag current-x current-y] coordinates
-          params (clj->js {:px            x
-                           :py            y
-                           :cx            current-x
-                           :cy            current-y
-                           :rx            rx
-                           :ry            ry
-                           :xAxisRotation x-axis-rotation
-                           :largeArcFlag  large-arc-flag
-                           :sweepFlag     sweep-flag})
-          curves (->> params ((.-default arcToBezier)) (js->clj))]
-      (map
-        (fn [{x1 "x1" y1 "y1" x2 "x2" y2 "y2" x3 "x" y3 "y"}]
-          {:bezier [{:x x1 :y y1} {:x x2 :y y2} {:x x3 :y y3}]})
-        curves))
-    (throw (js/Error. (str "Unknown path type: " path-type)))))
+  [{:keys [x y]} prev-path [path-type & coordinates] precision]
+  (let [->precision (partial (fn [precision coordinates]
+                               (-> coordinates
+                                   (update :x to-precision precision)
+                                   (update :y to-precision precision)))
+                             precision)]
+    (case path-type
+      ("M" "L")
+      (let [[x1 y1] coordinates]
+        [(->precision {:x x1 :y y1})])
+      ("H")
+      (let [[x1] coordinates]
+        [(->precision {:x x1 :y y})])
+      ("V")
+      (let [[y1] coordinates]
+        [(->precision {:x x :y y1})])
+      ("C")
+      (let [[x1 y1 x2 y2 x3 y3] coordinates]
+        [{:bezier [(->precision {:x x1 :y y1})
+                   (->precision {:x x2 :y y2})
+                   (->precision {:x x3 :y y3})]}])
+      ("S")
+      (let [[x2 y2 x3 y3] coordinates
+            prev-path-type (first prev-path)]
+        (if (contains? #{"C" "S"} prev-path-type)
+          (let [[x01 y01 x02 y02] (take-last 4 prev-path)
+                x1 (- (* 2 x02) x01)
+                y1 (- (* 2 y02) y01)]
+            [{:bezier [(->precision {:x x1 :y y1})
+                       (->precision {:x x2 :y y2})
+                       (->precision {:x x3 :y y3})]}])
+          [{:bezier [(->precision {:x x2 :y y2})
+                     (->precision {:x x2 :y y2})
+                     (->precision {:x x3 :y y3})]}]))
+      ("Q")
+      (let [[x1 y1 x2 y2] coordinates]
+        [{:bezier [(->precision {:x x1 :y y1})
+                   (->precision {:x x2 :y y2})]}])
+      ("T")
+      (let [[x2 y2] coordinates
+            prev-path-type (first prev-path)]
+        (if (contains? #{"Q"} prev-path-type)
+          (let [[x01 y01 x02 y02] (take-last 4 prev-path)
+                x1 (- (* 2 x02) x01)
+                y1 (- (* 2 y02) y01)]
+            [{:bezier [(->precision {:x x1 :y y1})
+                       (->precision {:x x2 :y y2})]}])
+          [(->precision {:x x2 :y y2})]))
+      ("A")
+      (let [[rx ry x-axis-rotation large-arc-flag sweep-flag current-x current-y] coordinates
+            params (clj->js {:px            x
+                             :py            y
+                             :cx            current-x
+                             :cy            current-y
+                             :rx            rx
+                             :ry            ry
+                             :xAxisRotation x-axis-rotation
+                             :largeArcFlag  large-arc-flag
+                             :sweepFlag     sweep-flag})
+            curves (->> params ((.-default arcToBezier)) (js->clj))]
+        (map
+          (fn [{x1 "x1" y1 "y1" x2 "x2" y2 "y2" x3 "x" y3 "y"}]
+            {:bezier [(->precision {:x x1 :y y1})
+                      (->precision {:x x2 :y y2})
+                      (->precision {:x x3 :y y3})]})
+          curves))
+      (throw (js/Error. (str "Unknown path type: " path-type))))))
 
 (defn- get-transitions
-  [options paths]
-  (->> paths
-       (reduce
-         (fn [[last-point prev-path result] path]
-           (let [transitions (get-transition last-point prev-path path)
-                 new-point (apply-path-to-point last-point path)]
-             [new-point path (concat result (map #(merge % options) transitions))]))
-         [{:x 0 :y 0} nil []])
-       (last)))
+  ([paths options]
+   (get-transitions paths options nil))
+  ([paths options precision]
+   (->> paths
+        (reduce
+          (fn [[last-point prev-path result] path]
+            (let [transitions (get-transition last-point prev-path path precision)
+                  new-point (apply-path-to-point last-point path)]
+              [new-point path (concat result (map #(merge % options) transitions))]))
+          [{:x 0 :y 0} nil []])
+        (last))))
 
 (defn- transition->path
   [transition]
@@ -130,7 +144,7 @@
       :else ["L" (:x transition) (:y transition)])))
 
 (defn- get-transitions-durations
-  [transitions total-duration origin]
+  [transitions total-duration origin precision]
   (let [lengths (->> transitions
                      (reduce
                        (fn [[{:keys [x y] :as last-point} result] transition]
@@ -142,17 +156,22 @@
                        [origin []])
                      (last))
         total-length (reduce + 0 lengths)]
-    (map #(* total-duration (/ % total-length)) lengths)))
+    (map #(-> (/ % total-length)
+              (* total-duration)
+              (to-precision precision))
+         lengths)))
 
 (defn path->transitions
-  [{:keys [path letter-path origin duration scale]}]
-  (let [absolute-paths (->> (if (some? letter-path) (get-svg-path letter-path {:trace? true}) path)
-                            (split-path)
-                            (map (partial apply-scale scale))
-                            (map (partial apply-origin origin)))
-        transitions (->> absolute-paths (get-transitions {}))
-        transition-durations (get-transitions-durations transitions duration origin)]
-    (map merge transitions (map #(hash-map :duration %) transition-durations))))
+  ([params]
+   (path->transitions params nil))
+  ([{:keys [path letter-path origin duration scale]} {:keys [precision] :or {precision 6}}]
+   (let [absolute-paths (->> (if (some? letter-path) (get-svg-path letter-path {:trace? true}) path)
+                             (split-path)
+                             (map (partial apply-scale scale))
+                             (map (partial apply-origin origin)))
+         transitions (get-transitions absolute-paths {} precision)
+         transition-durations (get-transitions-durations transitions duration origin precision)]
+     (map merge transitions (map #(hash-map :duration %) transition-durations)))))
 
 (defn- points-distance
   [p1 p2]
--- test/cljs/webchange/common/svg_path/path_to_transitions_test.cljs
+++ test/cljs/webchange/common/svg_path/path_to_transitions_test.cljs
@@ -13,16 +13,14 @@
     (is (= (apply-origin {:x 50 :y 70} ["Q" 30 40 50 40]) ["Q" 80 110 100 110]))
     (is (= (apply-origin {:x 50 :y 70} ["S" 30 40 50 40]) ["S" 80 110 100 110]))
     (is (= (apply-origin {:x 50 :y 70} ["T" 30 40]) ["T" 80 110]))
-    (is (= (apply-origin {:x 50 :y 70} ["A" 40 40 0 1 0 80 75]) ["A" 40 40 0 1 0 130 145]))
-    ))
+    (is (= (apply-origin {:x 50 :y 70} ["A" 40 40 0 1 0 80 75]) ["A" 40 40 0 1 0 130 145]))))
 
 (deftest test-apply-scale
   (let [apply-scale #'ptt/apply-scale]
     (is (= (apply-scale nil ["M" 80 25]) ["M" 80 25]))
     (is (= (apply-scale {:x 1 :y 1} ["M" 80 25]) ["M" 80 25]))
     (is (= (apply-scale {:x 2 :y 3} ["M" 80 25]) ["M" 160 75]))
-    (is (= (apply-scale {:x 2 :y 3} ["A" 40 40 0 1 0 80 75]) ["A" 80 120 0 1 0 160 225]))
-    ))
+    (is (= (apply-scale {:x 2 :y 3} ["A" 40 40 0 1 0 80 75]) ["A" 80 120 0 1 0 160 225]))))
 
 (deftest test-get-transition
   (let [get-transition #'ptt/get-transition]
@@ -50,48 +48,45 @@
            [{:bezier [{:x 69.3845832715486, :y 11.741348755731238} {:x 51.553475440960696, :y 6.632281187803727} {:x 35.52736115395997, :y 12.257453718120182}]}
             {:bezier [{:x 19.501246866959256, :y 17.882626248436637} {:x 8.77501000800801, :y 33.015333234477765} {:x 8.77501000800801, :y 49.99999999999999}]}
             {:bezier [{:x 8.77501000800801, :y 66.98466676552222} {:x 19.50124686695925, :y 82.11737375156335} {:x 35.52736115395997, :y 87.74254628187981}]}
-            {:bezier [{:x 51.55347544096068, :y 93.36771881219627} {:x 69.38458327154859, :y 88.25865124426878} {:x 80, :y 75.00000000000001}]}]))
-    ))
+            {:bezier [{:x 51.55347544096068, :y 93.36771881219627} {:x 69.38458327154859, :y 88.25865124426878} {:x 80, :y 75.00000000000001}]}]))))
 
 (deftest test-get-transitions
   (let [get-transitions #'ptt/get-transitions]
     (is (= (get-transitions
-             {:duration 100}
-             [["M" 80 25] ["L" 90 40] ["H" 95] ["V" 50]])
+             [["M" 80 25] ["L" 90 40] ["H" 95] ["V" 50]]
+             {:duration 100})
            [{:x 80 :y 25 :duration 100}
             {:x 90 :y 40 :duration 100}
             {:x 95 :y 40 :duration 100}
             {:x 95 :y 50 :duration 100}]))
     (is (= (get-transitions
-             {:duration 100}
-             [["M" 80 25] ["C" 30 40 50 40 60 30]])
+             [["M" 80 25] ["C" 30 40 50 40 60 30]]
+             {:duration 100})
            [{:x 80 :y 25 :duration 100}
             {:bezier [{:x 30 :y 40} {:x 50 :y 40} {:x 60 :y 30}] :duration 100}]))
     (is (= (get-transitions
-             {:duration 100}
-             [["M" 80 25] ["Q" 30 40 50 40]])
+             [["M" 80 25] ["Q" 30 40 50 40]]
+             {:duration 100})
            [{:x 80 :y 25 :duration 100}
             {:bezier [{:x 30 :y 40} {:x 50 :y 40}] :duration 100}]))
     (is (= (get-transitions
-             {:duration 100}
-             [["M" 80 25] ["C" 40 10 65 10 95 80] ["S" 150 150 180 80]])
+             [["M" 80 25] ["C" 40 10 65 10 95 80] ["S" 150 150 180 80]]
+             {:duration 100})
            [{:x 80 :y 25 :duration 100}
             {:bezier [{:x 40 :y 10} {:x 65 :y 10} {:x 95 :y 80}] :duration 100}
             {:bezier [{:x 125 :y 150} {:x 150 :y 150} {:x 180 :y 80}] :duration 100}]))
     (is (= (get-transitions
-             {:duration 100}
-             [["M" 80 25] ["Q" 40 10 65 10] ["T" 180 80]])
+             [["M" 80 25] ["Q" 40 10 65 10] ["T" 180 80]]
+             {:duration 100})
            [{:x 80 :y 25 :duration 100}
             {:bezier [{:x 40 :y 10} {:x 65 :y 10}] :duration 100}
-            {:bezier [{:x 90 :y 10} {:x 180 :y 80}] :duration 100}]))
-    ))
+            {:bezier [{:x 90 :y 10} {:x 180 :y 80}] :duration 100}]))))
 
 (deftest test-transition->path
   (let [transition->path #'ptt/transition->path]
     (is (= (transition->path {:x 260 :y 150}) ["L" 260 150]))
     (is (= (transition->path {:bezier [{:x 20 :y 20} {:x 40 :y 20} {:x 50 :y 10}]}) ["C" 20 20 40 20 50 10]))
-    (is (= (transition->path {:bezier [{:x 20 :y 20} {:x 40 :y 20}]}) ["Q" 20 20 40 20]))
-    ))
+    (is (= (transition->path {:bezier [{:x 20 :y 20} {:x 40 :y 20}]}) ["Q" 20 20 40 20]))))
 
 (deftest test-get-transitions-durations
   (let [get-transitions-durations #'ptt/get-transitions-durations]
@@ -101,37 +96,35 @@
               {:x 0 :y 100}]
              3
              {:x 0 :y 0})
-           [1 1 1]))
-    ))
+           [1 1 1]))))
 
 (deftest test-path->transitions
-  (is (= (ptt/path->transitions
-           {:path     "M 80 25 A 40,40 0 1,0 80,75 M 80 10 L 80 90"
-            :origin   {:x 1005 :y 585}
-            :scale    {:x 2 :y 2}
-            :duration 5})
-         [{:x 1165, :y 635, :duration 0.9834258124954962}
-          {:bezier [{:x 1143.7691665430973, :y 608.4826975114624}
-                    {:x 1108.1069508819214, :y 598.2645623756075}
-                    {:x 1076.05472230792, :y 609.5149074362404}],
-           :duration 0.5788134618388621}
-          {:bezier [{:x 1044.0024937339185, :y 620.7652524968732}
-                    {:x 1022.5500200160161, :y 651.0306664689556}
-                    {:x 1022.5500200160161, :y 685}],
-           :duration 0.5788126561802741}
-          {:bezier [{:x 1022.5500200160161, :y 718.9693335310444}
-                    {:x 1044.0024937339185, :y 749.2347475031268}
-                    {:x 1076.05472230792, :y 760.4850925637596}],
-           :duration 0.578812835215516}
-          {:bezier   [{:x 1108.1069508819214, :y 771.7354376243925}
-                      {:x 1143.7691665430973, :y 761.5173024885376}
-                      {:x 1165, :y 735}],
-           :duration 0.5788134618388621}
-          {:x 1165, :y 605, :duration 0.7626614841932022}
-          {:x 1165, :y 765, :duration 0.9386602882377875}]))
-  )
+  (let [result (ptt/path->transitions
+                 {:path     "M 80 25 A 40,40 0 1,0 80,75 M 80 10 L 80 90"
+                  :origin   {:x 1005 :y 585}
+                  :scale    {:x 2 :y 2}
+                  :duration 5})
+        expected-data [{:x 1165, :y 635, :duration 0.983425}
+                       {:bezier   [{:x 1143.769166, :y 608.482697}
+                                   {:x 1108.106950, :y 598.264562}
+                                   {:x 1076.054722, :y 609.514907}],
+                        :duration 0.578813}
+                       {:bezier   [{:x 1044.002493, :y 620.765252}
+                                   {:x 1022.550020, :y 651.030666}
+                                   {:x 1022.550020, :y 685}],
+                        :duration 0.578812}
+                       {:bezier   [{:x 1022.550020, :y 718.969333}
+                                   {:x 1044.002493, :y 749.234747}
+                                   {:x 1076.054722, :y 760.485092}],
+                        :duration 0.578812}
+                       {:bezier   [{:x 1108.106950, :y 771.735437}
+                                   {:x 1143.769166, :y 761.517302}
+                                   {:x 1165, :y 735}],
+                        :duration 0.578813}
+                       {:x 1165, :y 605, :duration 0.762661}
+                       {:x 1165, :y 765, :duration 0.938660}]]
+    (is (= result expected-data))))
 
 (deftest test-get-moves-lengths
   (is (= (ptt/get-moves-lengths "M 80 25 A 40,40 0 1,0 80,75 M 80 10 L 80 90") [83.81527307120105 65]))
-  (is (= (ptt/get-moves-lengths "M 80 0 L 80 90 m 100 0") [80 100]))
-  )
+  (is (= (ptt/get-moves-lengths "M 80 0 L 80 90 m 100 0") [80 100])))
