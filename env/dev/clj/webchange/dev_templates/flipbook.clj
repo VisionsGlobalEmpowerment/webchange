d41a470fb9a54f31105e27eacd25f8a2629e3524
Dmitrii Gorlov <wildeastengineer@gmail.com>
2022-02-02 14:35:05 +0400

    Fix apply origin

--- env/dev/clj/webchange/dev_templates/flipbook.clj
+++ env/dev/clj/webchange/dev_templates/flipbook.clj
@@ -18,3 +18,25 @@
   (core/save-scene! test-course-slug scene-slug stored-data t/user-id)
 
   (t/update-activity course-slug scene-slug))
+
+(comment
+  (defn- copy-activity
+    ([course-name-source activity-name]
+     (copy-activity course-name-source activity-name (-> (t/create-test-course) :slug)))
+    ([course-name-source activity-name course-name-source-new]
+     (let [scene-data (core/get-scene-latest-version course-name-source activity-name)]
+       (core/save-scene! course-name-source-new activity-name scene-data t/user-id)
+       [(str "http://localhost:3000/courses/" course-name-source-new "/editor-v2/" activity-name)
+        (str "http://localhost:3000/s/" course-name-source-new "/" activity-name)])))
+
+  (def course-slug-origin "sizwes-smile-english-hpiimczo")
+  (def scene-slug-origin "book")
+
+  (copy-activity course-slug-origin scene-slug-origin)
+
+  (def course-slug-copied "test-course-english-qitwmpwo")
+  (copy-activity course-slug-origin scene-slug-origin course-slug-copied)
+
+  (-> (core/get-scene-latest-version course-slug-copied scene-slug-origin)
+      (get-in [:objects])
+      (select-keys [:page-image-6 :page-47-image-2])))
