2d16bae0d367d33bbfd22034f8dbaa240f98025f
wildeastengineer <wildeastengineer@gmail.com>
2022-02-15 16:21:22 +0400

    Fix start activity (#776)
    * Start activity optimization
    
    * Fix

--- src/cljs/webchange/interpreter/renderer/stage.cljs
+++ src/cljs/webchange/interpreter/renderer/stage.cljs
@@ -68,10 +68,7 @@
   (let [{:keys [mode show-loader-screen?]
          :or   {show-loader-screen? true}} props]
     {:show-scene?          (and (some? viewport)
-                                (:done loading-state)
-                                (or (= mode ::modes/editor)
-                                    (= mode ::modes/preview)
-                                    (:started? scene-data)))
+                                (:done loading-state))
      :show-loader-screen?  (and show-loader-screen?
                                 (some? viewport)
                                 (or (not (:done loading-state))
@@ -89,7 +86,8 @@
                          :progress 0})
         current-scene-id (r/atom nil)
         handle-resize (get-handler handle-screen-resize container)
-        prev-viewport (atom nil)]
+        prev-viewport (atom nil)
+        scene-ready? (r/atom false)]
     (r/create-class
       {:display-name "web-gl-stage"
        :component-did-mount
@@ -130,13 +128,14 @@
                                     :objects  (:objects scene-data)
                                     :metadata (:metadata scene-data)
                                     :viewport viewport
-                                    :on-ready on-ready
-                                    :started? (:started? scene-data)}])
+                                    :on-ready #(do (reset! scene-ready? true)
+                                                   (on-ready))}])
                           (when show-loader-screen?
                             [overlay-wrapper {:viewport viewport}
                              [loader-screen {:on-start-click on-start-click
-                                             :loading        @loading}]])
+                                             :done           (and (:done @loading)
+                                                                  @scene-ready?)
+                                             :progress       (:progress @loading)}]])
                           (when show-waiting-screen?
                             [overlay-wrapper {:viewport viewport}
-                             [waiting-screen {:on-start-click on-start-click
-                                              :loading        @loading}]])]))})))
+                             [waiting-screen]])]))})))
