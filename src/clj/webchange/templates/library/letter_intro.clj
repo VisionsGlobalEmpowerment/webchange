d406052585b261d8d4088a3b409a80962b6b6c08
Dmitrii Gorlov <wildeastengineer@gmail.com>
2022-03-15 15:48:40 +0400

    Update font

--- src/clj/webchange/templates/library/letter_intro.clj
+++ src/clj/webchange/templates/library/letter_intro.clj
@@ -48,7 +48,7 @@
                                  :scene-name  "letter-big",
                                  :align       "center",
                                  :fill        "#ef545c",
-                                 :font-family "Lexend Deca",
+                                 :font-family "Tabschool",
                                  :font-size   120,
                                  :states      {:glow      {:permanent-pulsation {:speed 2}}
                                                :stop-glow {:permanent-pulsation false}
@@ -64,7 +64,7 @@
                                  :height      130,
                                  :align       "center",
                                  :fill        "#ef545c",
-                                 :font-family "Lexend Deca",
+                                 :font-family "Tabschool",
                                  :font-size   120,
                                  :states      {:glow      {:permanent-pulsation {:speed 2}}
                                                :stop-glow {:permanent-pulsation false}
@@ -122,7 +122,7 @@
                                  :align          "center"
                                  :vertical-align "bottom"
                                  :font-size      80
-                                 :font-family    "Lexend Deca"
+                                 :font-family    "Tabschool"
                                  :fill           "#fc8e51"
                                  :x              720
                                  :y              450
