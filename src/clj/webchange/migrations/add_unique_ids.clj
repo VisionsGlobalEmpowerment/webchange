4eae4a6bb64aa5f5f49cbb2f862b41dcabaea1f1
Ivan Khaldeev <ikhaldeev@yandex.ru>
2022-03-24 17:17:06 +0400

    Migrate spanish course to unique ids

--- src/clj/webchange/migrations/add_unique_ids.clj
+++ src/clj/webchange/migrations/add_unique_ids.clj
@@ -4,25 +4,25 @@
             [webchange.progress.core :as progress]
             [webchange.db.core :refer [*db*] :as db]))
 
-(defn add-unique-ids [levels unique-id]
+(defn- add-unique-ids [levels unique-id]
   (mapv (fn [level]
           (update-in level [:lessons]
-            (fn [l]
-              (mapv (fn [lesson]
-                      (update-in lesson [:activities]
-                        (fn [a]
-                          (mapv (fn [activity]
-                                  (assoc activity :unique-id
-                                    (swap! unique-id inc)))
-                            a)))) l)))) levels))
+                     (fn [l]
+                       (mapv (fn [lesson]
+                               (update-in lesson [:activities]
+                                          (fn [a]
+                                            (mapv (fn [activity]
+                                                    (assoc activity :unique-id
+                                                           (swap! unique-id inc)))
+                                                  a)))) l)))) levels))
 
-(defn map-map [func m]
+(defn- map-map [func m]
   (apply merge (map func m)))
 
-(defn k->n [k]
+(defn- k->n [k]
   (Integer/parseInt (name k)))
 
-(defn finished-index->unique-id [finished levels]
+(defn- finished-index->unique-id [finished levels]
   (map-map (fn [[level-key level-value]]
              {level-key
               (map-map (fn [[lesson-key lesson-value]]
@@ -31,31 +31,31 @@
                                   (get-in levels [(k->n level-key) :lessons
                                                   (k->n lesson-key) :activities
                                                   index :unique-id]))
-                            lesson-value)})
-                level-value)})
-    finished))
+                                lesson-value)})
+                       level-value)})
+           finished))
 
-(defn dissoc-in [map keys]
+(defn- dissoc-in [map keys]
   (if (= (count keys) 1)
     (dissoc map (nth keys 0))
     (update-in map (butlast keys) dissoc (last keys))))
 
-(defn update-progress [progress course-data]
+(defn- update-progress [progress course-data]
   (-> progress
-    (dissoc-in [:data :next])
-    (update-in [:data :finished] #(finished-index->unique-id % (:levels course-data)))))
+      (dissoc-in [:data :next])
+      (update-in [:data :finished] #(finished-index->unique-id % (:levels course-data)))))
 
-(defn migrate-up [config]
+(defn- migrate-up
+  [config course-slug]
   (mount/start)
   (let [unique-id (atom -1)
-        course-slug "english"
         course-id (:id (db/get-course {:slug course-slug}))]
     (if-let [new-course (db/get-latest-course-version {:course_id course-id})]
       (let [new-course (-> new-course
-                         (update-in [:data :levels] #(add-unique-ids % unique-id))
-                         (assoc-in [:data :unique-id] @unique-id))
+                           (update-in [:data :levels] #(add-unique-ids % unique-id))
+                           (assoc-in [:data :unique-id] @unique-id))
             new-progresses (->> (db/get-progress-for-course {:course_id course-id})
-                             (map #(update-progress % (:data new-course))))]
+                                (map #(update-progress % (:data new-course))))]
         (db/save-course-data! new-course)
         (doseq [{:keys [id data]} new-progresses]
           (progress/update-progress! id data)))
@@ -64,3 +64,11 @@
 (defn migrate-down [config]
   (mount/start)
   (print "migrate down"))
+
+(defn migrate-up-english
+  [config]
+  (migrate-up config "english"))
+
+(defn migrate-up-spanish
+  [config]
+  (migrate-up config "spanish"))
