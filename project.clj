5b5d27e9d4b43d3d7ad2f012b539429193430213
wee <wildeastengineer@gmail.com>
2022-03-30 11:33:22 +0400

    Remove re-frame-10x

--- project.clj
+++ project.clj
@@ -98,7 +98,6 @@
   :profiles
   {:dev
             {:dependencies [[binaryage/devtools "0.9.10"]
-                            [day8.re-frame/re-frame-10x "0.3.3"]
                             [day8.re-frame/tracing "0.5.1"]
                             [ring/ring-mock "0.3.2"]
                             [mockery "0.1.4"]
@@ -132,8 +131,7 @@
                     :output-dir           "resources/public/js/compiled/out"
                     :asset-path           "/js/compiled/out"
                     :source-map-timestamp true
-                    :preloads             [devtools.preload
-                                           day8.re-frame-10x.preload]
+                    :preloads             [devtools.preload]
                     :closure-defines      {"re_frame.trace.trace_enabled_QMARK_"        true
                                            "day8.re_frame.tracing.trace_enabled_QMARK_" true}
                     :external-config      {:devtools/config {:features-to-install :all}}
