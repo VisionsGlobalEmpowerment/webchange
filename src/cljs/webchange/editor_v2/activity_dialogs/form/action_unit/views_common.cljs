f46adf42afb5c586470cbf4b9235d10cb8a3181e
Ivan Khaldeev <ikhaldeev@yandex.ru>
2022-04-05 21:53:33 +0400

    Fix select text in phrase

--- src/cljs/webchange/editor_v2/activity_dialogs/form/action_unit/views_common.cljs
+++ src/cljs/webchange/editor_v2/activity_dialogs/form/action_unit/views_common.cljs
@@ -35,57 +35,62 @@
 (defn text-control
   [{:keys [on-change on-enter-press on-ctrl-enter-press placeholder on-parent-focus]}]
   (let [ref (atom nil)
-               current-value (atom nil)
-               empty-value? (r/atom false)
-               handle-key-down (fn [e]
-                                 (when (= (.-key e) "Enter")
-                                   (.preventDefault e)
-                                   (if (.-ctrlKey e)
-                                     (on-ctrl-enter-press)
-                                     (on-enter-press))))
-               check-empty-value! (fn []
-                                    (if (empty-text-value? @current-value)
-                                      (do (reset! empty-value? true)
-                                          (->> (or placeholder "Enter text")
-                                               (set! (.-innerText @ref))))
-                                      (reset! empty-value? false)))
-                handle-blur (fn [] (check-empty-value!))]
+        current-value (atom nil)
+        empty-value? (r/atom false)
+        handle-key-down (fn [e]
+                          (when (= (.-key e) "Enter")
+                            (.preventDefault e)
+                            (if (.-ctrlKey e)
+                              (on-ctrl-enter-press)
+                              (on-enter-press))))
+        try-placeholder! (fn []
+                           (if (empty-text-value? @current-value)
+                             (do (reset! empty-value? true)
+                                 (->> (or placeholder "Enter text")
+                                      (set! (.-innerText @ref))))
+                             (reset! empty-value? false)))
+        try-empty! (fn []
+                     (when @empty-value?
+                       (set! (.-innerText @ref) "")
+                       (reset! empty-value? false)))
+        check-and-select! (fn []
+                            (when @ref
+                              (try-empty!)
+                              (select-content-editable-text! @ref)))
+        handle-blur (fn [] (try-placeholder!))]
     (when on-parent-focus
-      (swap! on-parent-focus conj (fn []
-                                    (when @ref
-                                      (when @empty-value?
-                                        (set! (.-innerText @ref) "")
-                                        (reset! empty-value? false))
-                                      (select-content-editable-text! @ref)))))
+      (swap! on-parent-focus conj check-and-select!))
     (r/create-class
-      {:display-name "text-control"
-       :should-component-update
-                     (constantly false)
-       :component-did-mount
-                     (fn [this]
-                       (let [{:keys [editable? value]} (r/props this)]
-                         (reset! current-value value)
-                         (check-empty-value!)
-                         (when editable?
-                           (.addEventListener @ref "keydown" handle-key-down false))))
-       :component-will-unmount
-                     (fn []
-                       (.removeEventListener @ref "keydown" handle-key-down))
-       :reagent-render
-                     (fn [{:keys [value editable? placeholder]}]
-                       (let [show-placeholder? (nil? value)
-                             handle-change (fn [event]
-                                             (let [new-value (.. event -target -innerText)]
-                                               (reset! current-value new-value)
-                                               (on-change new-value)))]
-                         [:span (cond-> {:class-name (get-class-name {"text"          true
-                                                                      "text-disabled" (not editable?)
-                                                                      "placeholder"   @empty-value?})
-                                         :ref        #(when (some? %) (reset! ref %))
-                                         :on-blur    handle-blur}
-                                        editable? (merge {:on-input                          handle-change
-                                                          :content-editable                  true
-                                                          :suppress-content-editable-warning true}))
-                          (if show-placeholder?
-                            placeholder
-                            value)]))})))
+     {:display-name "text-control"
+      :should-component-update
+      (constantly false)
+      :component-did-mount
+      (fn [this]
+        (let [{:keys [editable? value]} (r/props this)]
+          (reset! current-value value)
+          (try-placeholder!)
+          (when editable?
+            (.addEventListener @ref "keydown" handle-key-down false))))
+      :component-will-unmount
+      (fn []
+        (.removeEventListener @ref "keydown" handle-key-down))
+      :reagent-render
+      (fn [{:keys [value editable? placeholder]}]
+        (let [show-placeholder? (nil? value)
+              handle-change (fn [event]
+                              (let [new-value (.. event -target -innerText)]
+                                (reset! current-value new-value)
+                                (on-change new-value)))]
+          [:span (cond-> {:class-name (get-class-name {"text"          true
+                                                       "text-disabled" (not editable?)
+                                                       "placeholder"   @empty-value?})
+                          :ref        #(when (some? %) (reset! ref %))
+                          :on-click #(do (try-empty!)
+                                         (.stopPropagation %))
+                          :on-blur    handle-blur}
+                         editable? (merge {:on-input                          handle-change
+                                           :content-editable                  true
+                                           :suppress-content-editable-warning true}))
+           (if show-placeholder?
+             placeholder
+             value)]))})))
