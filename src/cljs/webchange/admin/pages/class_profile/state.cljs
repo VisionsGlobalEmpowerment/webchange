3574f5e8eec39be1e64bcb5775870b399e6e9cf9
Ivan Khaldeev <ikhaldeev@yandex.ru>
2022-04-28 22:58:44 +0400

    State structure suggestion

--- src/cljs/webchange/admin/pages/class_profile/state.cljs
+++ src/cljs/webchange/admin/pages/class_profile/state.cljs
@@ -1,139 +1,89 @@
 (ns webchange.admin.pages.class-profile.state
   (:require
     [re-frame.core :as re-frame]
-    [webchange.admin.pages.state :as parent-state]
+    [re-frame.std-interceptors :as i]
     [webchange.state.warehouse :as warehouse]
     [webchange.validation.specs.class-spec :as class-spec]
     [webchange.validation.validate :refer [validate]]))
 
-(defn path-to-db
-  [relative-path]
-  (->> relative-path
-       (concat [:class-profile])
-       (parent-state/path-to-db)))
+(re-frame/reg-sub
+  ::class-profile
+  (fn [db]
+    (get-in db [:class-profile])))
+
+(defn- set-initial-class-data
+  [db class-data]
+  (assoc db :initial-class-data class-data))
+
+(defn- set-class-data
+  [db class-data]
+  (assoc db :class-data class-data))
+
+(defn- reset-class-data
+  [db class-data]
+  (-> db
+      (set-initial-class-data class-data)
+      (set-class-data class-data)))
+
+(defn- set-validation-errors
+  [db errors]
+  (assoc db :validation-errors errors))
 
 (re-frame/reg-event-fx
   ::init
-  (fn [{:keys [_]} [_ {:keys [class-id]}]]
-    {:dispatch-n [[::set-class-id {:class-id class-id}]
-                  [::warehouse/load-class {:class-id class-id}
-                   {:on-success [::load-class-success]}]]}))
+  [(i/path [:class-profile])]
+  (fn [{:keys [db]} [_ {:keys [class-id]}]]
+    {:db (assoc db :class-id class-id)
+     :dispatch [::warehouse/load-class {:class-id class-id}
+                {:on-success [::load-class-success]}]}))
 
 (re-frame/reg-event-fx
   ::load-class-success
-  (fn [{:keys [_]} [_ {:keys [class]}]]
+  [(i/path [:class-profile])]
+  (fn [{:keys [db]} [_ {:keys [class]}]]
     (let [class-data (select-keys class [:course-id :name])]
-      {:dispatch [::reset-class-data class-data]})))
-
-(re-frame/reg-event-fx
-  ::reset-class-data
-  (fn [{:keys [_]} [_ class-data]]
-    {:dispatch-n [[::set-initial-class-data class-data]
-                  [::set-class-data class-data]]}))
-
-;; Class id
-
-(def class-id-path (path-to-db [:class-id]))
-
-(defn- get-class-id
-  [db]
-  (get-in db class-id-path))
-
-(re-frame/reg-event-fx
-  ::set-class-id
-  (fn [{:keys [db]} [_ value]]
-    {:db (assoc-in db class-id-path value)}))
-
-;; Initial Class Data
-
-(def initial-class-data-path (path-to-db [:initial-class-data]))
-
-(re-frame/reg-sub
-  ::initial-class-data
-  (fn [db]
-    (get-in db initial-class-data-path)))
-
-(re-frame/reg-event-fx
-  ::set-initial-class-data
-  (fn [{:keys [db]} [_ value]]
-    {:db (assoc-in db initial-class-data-path value)}))
+      {:db (reset-class-data db class-data)})))
 
 ;; Class Data
 
-(def class-data-path (path-to-db [:class-data]))
-
 (re-frame/reg-event-fx
   ::set-class-data
+  [(i/path [:class-profile])]
   (fn [{:keys [db]} [_ value]]
     (let [validation-errors (validate ::class-spec/class value)]
-      {:db       (assoc-in db class-data-path value)
-       :dispatch [::set-validation-errors validation-errors]})))
-
-(re-frame/reg-event-fx
-  ::update-class-data
-  (fn [{:keys [db]} [_ data-patch]]
-    {:db (update-in db class-data-path merge data-patch)}))
-
-(defn- get-class-data
-  [db]
-  (get-in db class-data-path))
+      {:db       (-> db
+                     (set-class-data value)
+                     (set-validation-errors validation-errors))})))
 
 (re-frame/reg-sub
   ::class-data
-  get-class-data)
-
-(re-frame/reg-sub
-  ::has-changes?
-  (fn []
-    [(re-frame/subscribe [::class-data])
-     (re-frame/subscribe [::initial-class-data])])
-  (fn [[current-data initial-data]]
-    (not= current-data initial-data)))
-
-;; Validation
-
-(def validation-data-path (path-to-db [:validation-data]))
-
-(re-frame/reg-event-fx
-  ::set-validation-errors
-  (fn [{:keys [db]} [_ errors]]
-    {:db (assoc-in db validation-data-path errors)}))
-
-(re-frame/reg-event-fx
-  ::reset-validation-errors
-  (fn [{:keys [_]} [_]]
-    {:dispatch [::set-validation-errors nil]}))
-
-(re-frame/reg-sub
-  ::validation-errors
-  (fn [db]
-    (get-in db validation-data-path)))
+  :<- [::class-profile]
+  :class-data)
 
 ;; Save
 
 (re-frame/reg-sub
   ::save-button-enabled?
-  (fn []
-    [(re-frame/subscribe [::has-changes?])
-     (re-frame/subscribe [::validation-errors])])
-  (fn [[has-changes? validation-errors]]
-    (and has-changes?
+  :<- [::class-profile]
+  (fn [{:keys [current-data initial-data validation-errors]}]
+    (and (not= current-data initial-data)
          (not validation-errors))))
 
 (re-frame/reg-event-fx
   ::save-class
+  [(i/path [:class-profile])]
   (fn [{:keys [db]} [_]]
-    (let [{:keys [class-id]} (get-class-id db)
-          class-data (get-class-data db)
+    (let [{:keys [class-id class-data]} db
           validation-errors (validate ::class-spec/class class-data)]
       (if (nil? validation-errors)
-        {:dispatch-n [[::reset-validation-errors]
-                      [::warehouse/save-class {:class-id class-id
-                                               :data     class-data}
-                       {:on-success [::save-class-success]}]]}
-        {:dispatch [::set-validation-errors validation-errors]}))))
+        {:db (set-validation-errors db nil)
+         :dispatch [::warehouse/save-class {:class-id class-id
+                                            :data     class-data}
+                    {:on-success [::save-class-success]}]}
+        {:db (set-validation-errors db validation-errors)}))))
 
 (re-frame/reg-event-fx
   ::save-class-success
-  (fn [{:keys [_]} [_ {:keys [data]}]]
-    {:dispatch [::reset-class-data data]}))
+  [(i/path [:class-profile])]
+  (fn [{:keys [db]} [_ {:keys [data]}]]
+    {:db (reset-class-data db data)}))
