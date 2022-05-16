9e7ee52bce0a574db89c9b8a6118cbe808594ff2
wee <wildeastengineer@gmail.com>
2022-05-16 13:45:15 +0400

    Restore test page

--- src/cljs/webchange/ui_framework/test_page/index.cljs
+++ src/cljs/webchange/ui_framework/test_page/index.cljs
@@ -43,22 +43,53 @@
 (defn test-ui
   []
   [:div.test-ui-page
-   [:div.grid-sibling]
-   [:div.grid-container
-    [:div]
-    [:div]
-    [:div]
-    [:div.list
-     [:div.header]
-     [:div.content
-      [:div.list-item]
-      [:div.list-item]
-      [:div.list-item]
-      [:div.list-item]
-      [:div.list-item]
-      [:div.list-item]
-      [:div.list-item]
-      [:div.list-item]
-      [:div.list-item]
-      [:div.list-item]]
-     [:div.footer]]]])
+   [buttons]
+   [icons-group]
+   [icon-buttons-group]
+
+   ;[row {:title "Icon"}
+   ; [block {:title "With rotation"}
+   ;  [icon {:icon    "sync"
+   ;         :rotate? true}]]]
+
+
+   ;[row {:title "Icon Button"}
+   ; [block {:title "Default"}
+   ;  [icon-button {:icon "arrow-left"}]]
+   ; [block {:title "Primary Color"}
+   ;  [icon-button {:icon  "mic"
+   ;                :color "primary"}]]
+   ; [block {:title "Disabled"}
+   ;  [icon-button {:icon      "arrow-right"
+   ;                :disabled? true}]]
+   ; [block {:title "Outlined"}
+   ;  [icon-button {:icon    "link"
+   ;                :variant "outlined"}]]
+   ; [block {:title "Play button"}
+   ;  [icon-button {:icon  "play"
+   ;                :color "primary"}]]
+   ; [block {:title "With text"}
+   ;  [icon-button {:icon    "link"
+   ;                :variant "outlined"}
+   ;   "Copy Link"]]]
+
+   ;[:div.grid-sibling]
+   ;[:div.grid-container
+   ; [:div]
+   ; [:div]
+   ; [:div]
+   ; [:div.list
+   ;  [:div.header]
+   ;  [:div.content
+   ;   [:div.list-item]
+   ;   [:div.list-item]
+   ;   [:div.list-item]
+   ;   [:div.list-item]
+   ;   [:div.list-item]
+   ;   [:div.list-item]
+   ;   [:div.list-item]
+   ;   [:div.list-item]
+   ;   [:div.list-item]
+   ;   [:div.list-item]]
+   ;  [:div.footer]]]
+   ])
