30bf2f19fd6a3e81d05f1b463e0656a380085363
Ivan Khaldeev <ikhaldeev@yandex.ru>
2022-05-25 19:03:41 +0400

    fix build

--- src/clj/webchange/migrations/core.clj
+++ src/clj/webchange/migrations/core.clj
@@ -1,3 +1,4 @@
 (ns webchange.migrations.core
   (:require [webchange.migrations.add-unique-ids]
-            [webchange.migrations.change-first-word-book-template]))
+            [webchange.migrations.change-first-word-book-template]
+            [webchange.migrations.activity-stats-fill-unique-id]))
