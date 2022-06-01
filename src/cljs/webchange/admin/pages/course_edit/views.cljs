73ecb67107a355735067f20cc83382282f527eb1
wee <wildeastengineer@gmail.com>
2022-06-01 15:06:42 +0400

    DnD refactoring

--- src/cljs/webchange/admin/pages/course_edit/views.cljs
+++ src/cljs/webchange/admin/pages/course_edit/views.cljs
@@ -6,22 +6,7 @@
     [webchange.admin.pages.course-edit.state :as state]
     [webchange.admin.widgets.page.views :as page]
     [webchange.ui-framework.components.index :as ui]
-    [webchange.utils.drag-and-drop :as dnd]))
-
-(defonce dragged-item (atom {}))
-
-(defn- handle-drag-start
-  [event]
-  (reset! dragged-item (dnd/get-data-set event))
-  (set! (.. event -dataTransfer -effectAllowed) "move")
-  (set! (.. event -dataTransfer -dropEffect) "move")
-  (let [target (.-target event)]
-    (.add (.-classList target) "dragged")))
-
-(defn- handle-drag-over
-  [event]
-  (.preventDefault event)
-  (.stopPropagation event))
+    [webchange.utils.drag-and-drop :refer [draggable]]))
 
 (defn- drop-allowed?
   [dragged-data target-data]
@@ -29,51 +14,9 @@
           (:type dragged-data))
        (not= target-data dragged-data)))
 
-(defn- handle-drag-enter
-  [event]
-  (let [target-data (dnd/get-data-set event)]
-    (when (drop-allowed? @dragged-item target-data)
-      (.add (.. event -target -classList) "drag-over"))))
-
-(defn- handle-drag-leave
-  [event]
-  (let [target (.-target event)]
-    (.remove (.-classList target) "drag-over")))
-
-(defn- handle-drag-end
-  [event]
-  (let [target (.-target event)
-        all-items (.querySelectorAll js/document "[draggable]")]
-    (.remove (.-classList target) "dragged")
-    (.forEach all-items (fn [item]
-                          (.remove (.-classList item) "drag-over")))))
-
 (defn- handle-drop
-  [event]
-  (let [target-data (dnd/get-data-set event)]
-    (when (drop-allowed? @dragged-item target-data)
-      (js/console.log "handle-drop")
-      (print "target" (dnd/get-data-set event))
-      (print "dragged" @dragged-item))))
-
-(defn- init-dnd
-  [el]
-  (.addEventListener el "dragstart" handle-drag-start)
-  (.addEventListener el "dragover" handle-drag-over)
-  (.addEventListener el "dragenter" handle-drag-enter)
-  (.addEventListener el "dragleave" handle-drag-leave)
-  (.addEventListener el "dragend" handle-drag-end)
-  (.addEventListener el "drop" handle-drop))
-
-(defn- reset-dnd
-  [el]
-  (.removeEventListener el "dragstart" handle-drag-start)
-  (.removeEventListener el "dragover" handle-drag-over)
-  (.removeEventListener el "dragenter" handle-drag-enter)
-  (.removeEventListener el "dragleave" handle-drag-leave)
-  (.removeEventListener el "dragend" handle-drag-end)
-  (.removeEventListener el "drop" handle-drop))
-
+  [props]
+  (print ">>> Handle Drop" props))
 
 (defn- reorder-control
   []
@@ -108,29 +51,27 @@
   (r/with-let [expanded? (r/atom false)
                handle-item-click #(swap! expanded? not)
                handle-remove-click #(do (.stopPropagation %)
-                                        (re-frame/dispatch [::state/remove-lesson level-idx idx]))
-               handle-ref #(when (some? %)
-                             (init-dnd %))]
+                                        (re-frame/dispatch [::state/remove-lesson level-idx idx]))]
     [:<>
-     [l/list-item {:name       name
-                   :class-name (ui/get-class-name {"list-item" true
-                                                   "selected"  @expanded?})
-                   :on-click   handle-item-click
-                   :pre        [reorder-control]
-                   :html-attrs {:draggable   true
-                                :data-type   "lesson"
-                                :data-level  level-idx
-                                :data-lesson idx}
-                   :actions    [ui/icon-button {:icon     "remove"
-                                                :title    "Remove"
-                                                :variant  "light"
-                                                :on-click handle-remove-click}]
-                   :ref        handle-ref}
-      [l/content-right
-       [:div.list-item-stats
-        activities-number
-        [ui/icon {:icon       "activity"
-                  :class-name "activity"}]]]]
+     [draggable {:data          {:type   "lesson"
+                                 :level  level-idx
+                                 :lesson idx}
+                 :drop-allowed? drop-allowed?
+                 :on-drop       handle-drop}
+      [l/list-item {:name       name
+                    :class-name (ui/get-class-name {"list-item" true
+                                                    "selected"  @expanded?})
+                    :on-click   handle-item-click
+                    :pre        [reorder-control]
+                    :actions    [ui/icon-button {:icon     "remove"
+                                                 :title    "Remove"
+                                                 :variant  "light"
+                                                 :on-click handle-remove-click}]}
+       [l/content-right
+        [:div.list-item-stats
+         activities-number
+         [ui/icon {:icon       "activity"
+                   :class-name "activity"}]]]]]
      (when @expanded?
        [activities-list {:level-idx  level-idx
                          :lesson-idx idx}])]))
@@ -148,28 +89,26 @@
   (r/with-let [expanded? (r/atom false)
                handle-item-click #(swap! expanded? not)
                handle-remove-click #(do (.stopPropagation %)
-                                        (re-frame/dispatch [::state/remove-level idx]))
-               handle-ref #(when (some? %)
-                             (init-dnd %))]
+                                        (re-frame/dispatch [::state/remove-level idx]))]
     [:<>
-     [l/list-item {:name       name
-                   :class-name (ui/get-class-name {"list-item" true
-                                                   "selected"  @expanded?})
-                   :on-click   handle-item-click
-                   :pre        [reorder-control]
-                   :html-attrs {:draggable  true
-                                :data-type  "level"
-                                :data-level idx}
-                   :actions    [ui/icon-button {:icon     "remove"
-                                                :title    "Remove"
-                                                :variant  "light"
-                                                :on-click handle-remove-click}]
-                   :ref        handle-ref}
-      [l/content-right
-       [:div.list-item-stats
-        lessons-number
-        [ui/icon {:icon        "lesson"
-                  ::class-name "lesson"}]]]]
+     [draggable {:data          {:type  "level"
+                                 :level idx}
+                 :drop-allowed? drop-allowed?
+                 :on-drop       handle-drop}
+      [l/list-item {:name       name
+                    :class-name (ui/get-class-name {"list-item" true
+                                                    "selected"  @expanded?})
+                    :on-click   handle-item-click
+                    :pre        [reorder-control]
+                    :actions    [ui/icon-button {:icon     "remove"
+                                                 :title    "Remove"
+                                                 :variant  "light"
+                                                 :on-click handle-remove-click}]}
+       [l/content-right
+        [:div.list-item-stats
+         lessons-number
+         [ui/icon {:icon        "lesson"
+                   ::class-name "lesson"}]]]]]
      (when @expanded?
        [lessons-list {:level-idx idx}])]))
 
--- src/cljs/webchange/utils/drag_and_drop.cljs
+++ src/cljs/webchange/utils/drag_and_drop.cljs
@@ -1,8 +1,16 @@
-(ns webchange.utils.drag-and-drop)
+(ns webchange.utils.drag-and-drop
+  (:require
+    [reagent.core :as r]))
+
+(defn- event->target
+  [event]
+  (-> (.-target event)
+      (.closest "[draggable]")))
 
 (defn- get-data-set
   [event]
-  (->> (.. event -target -dataset)
+  (->> (event->target event)
+       (.-dataset)
        (js/Object.entries)
        (js->clj)
        (map (fn [[field value]]
@@ -27,6 +35,104 @@
     (doseq [key (keys data)]
       (.setData data-transfer (clojure.core/name key) (get data key)))))
 
+;;
+
+(defn- event->wrapped-child
+  [event]
+  (.. event -target -firstChild))
+
+(defn- add-class
+  [el class-name]
+  (-> (.-classList el)
+      (.add class-name)))
+
+(defn- remove-class
+  [el class-name]
+  (-> (.-classList el)
+      (.remove class-name)))
+
+(defonce dragged-item (atom {}))
+(defonce hover-counter (atom 0))
+
+(defn- handle-drag-start
+  [event]
+  (reset! dragged-item (get-data-set event))
+  (set! (.. event -dataTransfer -effectAllowed) "move")
+  (set! (.. event -dataTransfer -dropEffect) "move")
+  (-> (event->target event)
+      (add-class "dragged")))
+
+(defn- handle-drag-over
+  [event]
+  (.preventDefault event)
+  (.stopPropagation event))
+
+(defn- handle-drag-enter
+  [drop-allowed? event]
+  (swap! hover-counter inc)
+  (let [target-data (get-data-set event)]
+    (when (drop-allowed? @dragged-item target-data)
+      (-> (event->target event)
+          (add-class "drag-over")))))
+
+(defn- handle-drag-leave
+  [event]
+  (swap! hover-counter dec)
+  (when (= @hover-counter 0)
+    (-> (event->target event)
+        (remove-class "drag-over"))))
+
+(defn- handle-drag-end
+  [event]
+  (reset! hover-counter 0)
+  (let [target (.-target event)
+        all-items (.querySelectorAll js/document "[draggable]")]
+    (.remove (.-classList target) "dragged")
+    (.forEach all-items (fn [item]
+                          (.remove (.-classList item) "drag-over")))))
+
+(defn- handle-drop
+  [drop-allowed? on-drop event]
+  (let [target-data (get-data-set event)]
+    (when (and (fn? on-drop)
+               (drop-allowed? @dragged-item target-data))
+      (on-drop {:dragged @dragged-item
+                :target  (get-data-set event)}))))
+
+(defn- init-dnd
+  [el handlers]
+  (doseq [[event handler] handlers]
+    (.addEventListener el event handler)))
+
+(defn- reset-dnd
+  [el handlers]
+  (doseq [[event handler] handlers]
+    (.removeEventListener el event handler)))
+
+(defn- get-data-attrs
+  [data]
+  (->> data
+       (map (fn [[field-name field-value]]
+              [(->> field-name name (str "data-") keyword) field-value]))
+       (into {})))
+
 (defn draggable
-  []
-  )
+  [{:keys [data drop-allowed? on-drop]}]
+  (r/with-let [el (atom nil)
+               handlers {"dragstart" handle-drag-start
+                         "dragover"  handle-drag-over
+                         "dragenter" (partial handle-drag-enter drop-allowed?)
+                         "dragleave" handle-drag-leave
+                         "dragend"   handle-drag-end
+                         "drop"      (partial handle-drop drop-allowed? on-drop)}
+               handle-ref #(when (some? %)
+                             (reset! el %)
+                             (init-dnd @el handlers))]
+    (->> (r/current-component)
+         (r/children)
+         (into [:div (merge {:draggable  true
+                             :ref        handle-ref
+                             :class-name "wc-draggable"}
+                            (get-data-attrs data))]))
+    (finally
+      (reset-dnd @el handlers))))
