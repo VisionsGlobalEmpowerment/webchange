23b22e167d2a7dd912b7b09df98c6d20b2e362b1
wee <wildeastengineer@gmail.com>
2022-06-10 14:36:33 +0400

    Course finish spanish

--- src/cljs/webchange/student_dashboard/timeline/views_great_work.cljs
+++ src/cljs/webchange/student_dashboard/timeline/views_great_work.cljs
@@ -1,5 +1,8 @@
-(ns webchange.student-dashboard.timeline.views-great-work)
+(ns webchange.student-dashboard.timeline.views-great-work
+  (:require
+    [webchange.ui-framework.components.utils :as ui]))
 
 (defn great-work-button
-  []
-  [:div.great-work-button])
+  [{:keys [lang]}]
+  [:div {:class-name (ui/get-class-name {"great-work-button" true
+                                         (str "lang-" lang)  (some? lang)})}])
