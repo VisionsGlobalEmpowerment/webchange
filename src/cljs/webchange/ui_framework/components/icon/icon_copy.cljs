c9fa749cedb37b47f28ec94366e0797e71e86a5e
wee <wildeastengineer@gmail.com>
2022-06-13 09:35:50 +0400

    Fix copy icon

--- src/cljs/webchange/ui_framework/components/icon/icon_copy.cljs
+++ src/cljs/webchange/ui_framework/components/icon/icon_copy.cljs
@@ -1,12 +1,11 @@
 (ns webchange.ui-framework.components.icon.icon-copy)
 
 (def data
-  [:svg {:width   "18"
-         :height  "18"
-         :viewBox "0 0 18 18"
-         :fill    "none"
-         :xmlns   "http://www.w3.org/2000/svg"}
-   [:path {:d "M11.8125 11.812H15.1875V2.81201H6.1875V6.18701"
-           :stroke-width "2" :stroke-linecap "round" :stroke-linejoin "round"}]
-   [:path {:d "M11.8125 6.18701H2.8125V15.187H11.8125V6.18701Z"
-           :stroke-width "2" :stroke-linecap "round" :stroke-linejoin "round"}]])
+  [:svg {:width      "18"
+         :height     "18"
+         :viewBox    "0 0 18 18"
+         :class-name "stroke-colored"
+         :fill       "none" :stroke-width "2" :stroke-linecap "round" :stroke-linejoin "round"
+         :xmlns      "http://www.w3.org/2000/svg"}
+   [:path {:d "M11.8125 11.812H15.1875V2.81201H6.1875V6.18701"}]
+   [:path {:d "M11.8125 6.18701H2.8125V15.187H11.8125V6.18701Z"}]])
