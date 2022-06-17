be577d4949b47efc318ef4f2c3174648ac3ced59
wee <wildeastengineer@gmail.com>
2022-06-17 14:15:08 +0400

    Rename add school page

--- src/cljs/webchange/admin/pages/school_add/views.cljs
+++ src/cljs/webchange/admin/pages/school_add/views.cljs
@@ -1,6 +1,6 @@
-(ns webchange.admin.pages.add-school.views
+(ns webchange.admin.pages.school-add.views
   (:require [re-frame.core :as re-frame]
-            [webchange.admin.pages.add-school.state :as state]
+            [webchange.admin.pages.school-add.state :as state]
             [webchange.admin.widgets.page.views :as page]
             [webchange.admin.widgets.school-form.views :refer [add-school-form]]
             [webchange.ui-framework.components.index :as ui]))
