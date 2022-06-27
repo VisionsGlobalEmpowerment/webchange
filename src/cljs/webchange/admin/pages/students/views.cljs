790ae3f51e966577ef18dbb760a40d3a7a90154e
wee <wildeastengineer@gmail.com>
2022-06-27 15:36:15 +0400

    Classes list

--- src/cljs/webchange/admin/pages/students/views.cljs
+++ src/cljs/webchange/admin/pages/students/views.cljs
@@ -5,27 +5,6 @@
     [webchange.admin.widgets.page.views :as page]
     [webchange.ui.index :as ui]))
 
-
-
-#_(defn- list-item
-  [{:keys [id] :as props}]
-  (let [handle-edit-click #(re-frame/dispatch [::state/edit-student id])
-        remove-student #(re-frame/dispatch [::state/remove-student id])
-        handle-remove-click #(ui/with-confirmation {:message    "Remove Student?"
-                                                    :on-confirm remove-student})
-        student-removing? @(re-frame/subscribe [::state/student-removing? id])]
-    [l/list-item (merge props
-                        {:actions [:<>
-                                   [ui/icon-button {:icon     "remove"
-                                                    :title    "Remove"
-                                                    :variant  "light"
-                                                    :loading? student-removing?
-                                                    :on-click handle-remove-click}]
-                                   [ui/icon-button {:icon     "edit"
-                                                    :title    "Edit"
-                                                    :variant  "light"
-                                                    :on-click handle-edit-click}]]})]))
-
 (defn- list-item
   [{:keys [active? email id last-login name]}]
   (let [handle-edit-click #(re-frame/dispatch [::state/edit-student id])
