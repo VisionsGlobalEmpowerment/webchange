785066ad1f0aedcd7a56ca377960d1f59817eaaf
wee <wildeastengineer@gmail.com>
2022-08-25 16:44:21 +0400

    Sign in form styles

--- src/cljs/webchange/student/pages/sign_in/views.cljs
+++ src/cljs/webchange/student/pages/sign_in/views.cljs
@@ -1,41 +1,46 @@
 (ns webchange.student.pages.sign-in.views
   (:require
     [re-frame.core :as re-frame]
-    [webchange.student.pages.sign-in.state :as state]))
+    [webchange.student.pages.sign-in.state :as state]
+    [webchange.ui.utils.get-class-name :refer [get-class-name]]))
 
 (defn- current-code-item
   [{:keys [value]}]
-  (if (some? value)
-    value "*"))
+  [:div {:class-name (get-class-name {"current-code-item"        true
+                                      "current-code-item--empty" (nil? value)})}
+   (when (some? value)
+     value)])
 
 (defn- current-code
   []
   (let [code @(re-frame/subscribe [::state/code])
         handle-remove-click #(re-frame/dispatch [::state/remove-value])]
-    [:div
+    [:div {:class-name "current-code"}
      (for [value code]
        ^{:key value}
        [current-code-item {:value value}])
-     [:button {:on-click handle-remove-click}
-      "<<"]]))
+     [:button {:class-name "remove-button"
+               :on-click   handle-remove-click}]]))
 
 (defn- num-pad-key
   [{:keys [value]}]
   (let [handle-click #(re-frame/dispatch [::state/enter-value value])]
-    [:button {:on-click handle-click}
+    [:button {:on-click   handle-click
+              :class-name (get-class-name {"num-pad-key"                     true
+                                           (str "num-pad-key--value-" value) true})}
      value]))
 
 (defn- num-pad
   []
-  [:div
+  [:div.num-pad
    (for [value [1 2 3 4 5 6 7 8 9 0]]
      ^{:key value}
      [num-pad-key {:value value}])])
 
 (defn- sign-in-form
   []
-  [:div
-   [:h1 "Student Access"]
+  [:div {:class-name "sign-in-form"}
+   [:h1.header "Student Access"]
    [current-code]
    [num-pad]])
 
