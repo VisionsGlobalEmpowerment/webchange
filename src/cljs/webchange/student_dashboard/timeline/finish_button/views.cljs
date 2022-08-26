89c7fc17af956276255cfef4806806d7fbfd3c56
wee <wildeastengineer@gmail.com>
2022-08-26 14:51:18 +0400

    Fix student timeline

---
+++ src/cljs/webchange/student_dashboard/timeline/finish_button/views.cljs
@@ -0,0 +1,12 @@
+(ns webchange.student-dashboard.timeline.finish-button.views
+  (:require
+    [webchange.ui-framework.components.utils :as ui]))
+
+(def img-src {"english" "/images/student_dashboard/good_work_eng.png"
+              "spanish" "/images/student_dashboard/good_work_spa.png"})
+
+(defn finish-button
+  [{:keys [lang]}]
+  [:div {:class-name (ui/get-class-name {"finish-button" true})}
+   [:img {:src (or (get img-src lang)
+                   (get img-src "english"))}]])
