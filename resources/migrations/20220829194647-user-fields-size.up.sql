430a4592446eedcf5facde4bcdf9559eb336caa3
Ivan Khaldeev <ikhaldeev@yandex.ru>
2022-08-29 23:48:53 +0400

    Update user fields

---
+++ resources/migrations/20220829194647-user-fields-size.up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE users ALTER COLUMN first_name TYPE varchar(100);
+--;;
+ALTER TABLE users ALTER COLUMN last_name TYPE varchar(100);
+--;;
+ALTER TABLE users ALTER COLUMN email TYPE varchar(200);
+--;;
