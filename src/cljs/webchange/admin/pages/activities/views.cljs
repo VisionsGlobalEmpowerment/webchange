7688f1ecb5f742cd08ffe5227d2ed093fc4851ec
wildeastengineer <wildeastengineer@gmail.com>
2022-08-31 16:23:52 +0400

    Update global activities selector label (#1043)

--- src/cljs/webchange/admin/pages/activities/views.cljs
+++ src/cljs/webchange/admin/pages/activities/views.cljs
@@ -33,6 +33,28 @@
              :on-change  handle-change
              :class-name "activities-search-input"}]))
 
+(defn- my-global-switcher
+  []
+  (let [show-my-global? @(re-frame/subscribe [::state/show-my-global?])
+        handle-change #(re-frame/dispatch [::state/set-show-global (not show-my-global?)])]
+    [ui/switch {:label      (if show-my-global?
+                              "Hide My Global Activities"
+                              "Show My Global Activities")
+                :checked?   show-my-global?
+                :on-change  handle-change
+                :color      "yellow-1"
+                :class-name "show-global-selector"}]))
+
+(defn- language-selector
+  []
+  (let [current-language @(re-frame/subscribe [::state/current-language])
+        handle-select-language #(re-frame/dispatch [::state/select-language %])]
+    [ui/select {:label      "Language"
+                :value      current-language
+                :options    language-options
+                :on-change  handle-select-language
+                :class-name "language-selector" }]))
+
 (defn page
   [props]
   (re-frame/dispatch [::state/init props])
@@ -40,25 +62,15 @@
     (let [activities @(re-frame/subscribe [::state/activities])
           loading? @(re-frame/subscribe [::state/activities-loading?])
           handle-card-click #(re-frame/dispatch [::state/open-activity %])
-          handle-edit-click #(re-frame/dispatch [::state/edit-activity %])
-          current-language @(re-frame/subscribe [::state/current-language])
-          handle-select-language #(re-frame/dispatch [::state/select-language %])
-
-          show-my-global? @(re-frame/subscribe [::state/show-my-global?])]
+          handle-edit-click #(re-frame/dispatch [::state/edit-activity %])]
       [page/single-page {:class-name "page--activities"
                          :search     [search-bar]
                          :header     {:title      "Activities"
                                       :icon       "games"
                                       :icon-color "blue-2"
                                       :controls   [[activity-type-selector]
-                                                   [ui/switch {:class-name "show-global-selector"
-                                                               :label      "Show My Global Activities"
-                                                               :checked?   show-my-global?
-                                                               :on-change  #(re-frame/dispatch [::state/set-show-global (not show-my-global?)])}]
-                                                   [ui/select {:label     "Language"
-                                                               :value     current-language
-                                                               :options   language-options
-                                                               :on-change handle-select-language}]]}}
+                                                   [my-global-switcher]
+                                                   [language-selector]]}}
        [activities-list {:data                   activities
                          :loading?               loading?
                          :on-activity-click      handle-card-click
