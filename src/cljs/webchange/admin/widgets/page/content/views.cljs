f8d13bad899ec74b5835a5f4d13aca3aeceeb62e
wee <wildeastengineer@gmail.com>
2022-08-31 10:45:44 +0400

    Fix styles

--- src/cljs/webchange/admin/widgets/page/content/views.cljs
+++ src/cljs/webchange/admin/widgets/page/content/views.cljs
@@ -7,12 +7,13 @@
 (def component-class-name "widget--side-bar-page--main-content")
 
 (defn content
-  [{:keys [class-name transparent?] :as props}]
+  [{:keys [class-name class-name-content transparent?] :as props}]
   (->> (r/current-component)
        (r/children)
        (into [block (merge props
-                           {:class-name         (get-class-name {component-class-name true
+                           {:class-name         (get-class-name {component-class-name                       true
                                                                  (str component-class-name "--transparent") transparent?
-                                                                 class-name           (some? class-name)})
+                                                                 class-name                                 (some? class-name)})
 
-                            :class-name-content "widget--side-bar-page--main-content--content"})])))
+                            :class-name-content (get-class-name {"widget--side-bar-page--main-content--content" true
+                                                                 class-name-content                             (some? class-name-content)})})])))
