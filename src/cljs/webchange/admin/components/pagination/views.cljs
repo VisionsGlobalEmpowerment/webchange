416b17cd17a9719c65037a7d9d0dadbd1d6c1913
wildeastengineer <wildeastengineer@gmail.com>
2022-09-05 14:40:08 +0400

    Available activities pagination (#1050)

--- src/cljs/webchange/admin/components/pagination/views.cljs
+++ src/cljs/webchange/admin/components/pagination/views.cljs
@@ -1,34 +1,19 @@
 (ns webchange.admin.components.pagination.views
   (:require
-    [re-frame.core :as re-frame]
-    [reagent.core :as r]
-    [webchange.admin.components.pagination.state :as state]
-    [webchange.ui-framework.components.index :as ui]))
-
-(defn- page-item
-  [{:keys [active? on-click title value]}]
-  (let [handle-click #(when (and (not active?)
-                                 (fn? on-click))
-                        (on-click %))]
-    [ui/button {:class-name (ui/get-class-name {"pagination--button" true
-                                                "active"             active?})
-                :on-click   #(handle-click value)}
-     title]))
+    [webchange.ui.index :as ui]))
 
 (defn pagination
-  []
-  (r/create-class
-    {:display-name "Pagination"
-
-     :component-did-mount
-     (fn [this]
-       (re-frame/dispatch [::state/init (r/props this)]))
-
-     :reagent-render
-     (fn [{:keys [on-click]}]
-       (let [buttons @(re-frame/subscribe [::state/buttons])]
-         [:div.component--pagination
-          (for [{:keys [value] :as button} buttons]
-            ^{:key value}
-            [page-item (assoc button
-                         :on-click on-click)])]))}))
+  [{:keys [current total on-click]}]
+  (let [handle-click #(when (fn? on-click) (on-click %))
+        handle-next-click #(handle-click (inc current))
+        handle-prev-click #(handle-click (dec current))]
+    [:div.component--pagination
+     [ui/button {:icon      "caret-left"
+                 :disabled? (<= current 1)
+                 :on-click  handle-prev-click
+                 :color     "blue-1"}]
+     [:label "Page " [:b current] " of " total]
+     [ui/button {:icon      "caret-right"
+                 :disabled? (>= current total)
+                 :on-click  handle-next-click
+                 :color     "blue-1"}]]))
