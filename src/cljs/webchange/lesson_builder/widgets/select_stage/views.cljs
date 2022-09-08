b2c5a2c81eae95a6df5354c986a9fc4be5e3ff07
wee <wildeastengineer@gmail.com>
2022-09-08 12:38:25 +0400

    Update prev/next stage buttons

--- src/cljs/webchange/lesson_builder/widgets/select_stage/views.cljs
+++ src/cljs/webchange/lesson_builder/widgets/select_stage/views.cljs
@@ -1,23 +1,22 @@
 (ns webchange.lesson-builder.widgets.select-stage.views
   (:require
     [re-frame.core :as re-frame]
-    [webchange.lesson-builder.widgets.select-stage.state :as state]
+    [webchange.lesson-builder.state-flipbook :as state]
     [webchange.ui.index :as ui]))
 
 (defn select-stage
   [{:keys [class-name]}]
-  (let [has-prev-stage? @(re-frame/subscribe [::state/has-prev-stage?])
-        open-next-stage #(re-frame/dispatch [::state/open-next-stage])
-        open-prev-stage #(re-frame/dispatch [::state/open-prev-stage])]
+  (let [has-next-stage? @(re-frame/subscribe [::state/has-next-stage?])
+        has-prev-stage? @(re-frame/subscribe [::state/has-prev-stage?])
+        open-next-stage #(re-frame/dispatch [::state/show-next-stage])
+        open-prev-stage #(re-frame/dispatch [::state/show-prev-stage])]
     [:div {:class-name (ui/get-class-name {"widget--select-page" true
                                            class-name            (some? class-name)})}
      [ui/button {:icon      "arrow-left"
                  :color     "blue-1"
                  :disabled? (not has-prev-stage?)
                  :on-click  open-prev-stage}]
-     [ui/button {:icon     "arrow-right"
-                 :color    "blue-1"
-                 :on-click open-next-stage}]]))
-
-
-
+     [ui/button {:icon      "arrow-right"
+                 :color     "blue-1"
+                 :disabled? (not has-next-stage?)
+                 :on-click  open-next-stage}]]))
