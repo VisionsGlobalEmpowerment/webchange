5fa7df188f4145d34609867005de161f547f4805
wee <wildeastengineer@gmail.com>
2022-09-15 16:15:54 +0400

    Update build actions for flipbook

--- src/cljs/webchange/lesson_builder/widgets/activity_actions/state.cljs
+++ src/cljs/webchange/lesson_builder/widgets/activity_actions/state.cljs
@@ -2,26 +2,45 @@
   (:require
     [re-frame.core :as re-frame]
     [webchange.lesson-builder.blocks.menu.state :as menu]
-    [webchange.lesson-builder.blocks.state :as layout-state]))
+    [webchange.lesson-builder.blocks.state :as layout-state]
+    [webchange.lesson-builder.state :as state]))
 
 (re-frame/reg-sub
   ::menu-items
-  (constantly [{:id   :image-add
-                :text "Add Image"
-                :icon "plus"}
-               {:id   :character-add
-                :text "Add Character"
-                :icon "plus"}
-               {:id   :background-image
-                :text "Background"
-                :icon "plus"}
-               {:id   :background-music
-                :text "Background Music"
-                :icon "plus"}]))
+  :<- [::state/flipbook?]
+  (fn [flipbook?]
+    (cond-> []
+            flipbook? (concat [#_{:id   :edit-cover
+                                  :text "Edit Cover"
+                                  :icon "edit"}
+                               #_{:id   :add-empty-page
+                                  :text "Add Empty Page"
+                                  :icon "plus"}
+                               {:id   :add-layout
+                                :text "Add Layout"
+                                :icon "plus"}])
+            :always (concat [
+                             #_{:id   :image-text
+                                :text "Add Text"
+                                :icon "plus"}
+                             ])
+            (not flipbook?) (concat [{:id   :image-add
+                                      :text "Add Image"
+                                      :icon "plus"}
+                                     {:id   :character-add
+                                      :text "Add Character"
+                                      :icon "plus"}
+                                     {:id   :background-image
+                                      :text "Background"
+                                      :icon "plus"}])
+            :always (concat [{:id   :background-music
+                              :text "Background Music"
+                              :icon "plus"}]))))
 
 (re-frame/reg-event-fx
   ::handle-item-click
   (fn [{:keys [_]} [_ component-name]]
     {:dispatch (cond
+                 (= component-name :add-layout) [::layout-state/open-tool :add-page]
                  (= component-name :background-image) [::layout-state/open-tool :background-image]
                  :default [::menu/open-component component-name])}))
