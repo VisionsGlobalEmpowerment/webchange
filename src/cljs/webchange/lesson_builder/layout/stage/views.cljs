5c30d004fe43738323ad96956ff9c9fd15faf58b
Ivan Khaldeev <ikhaldeev@yandex.ru>
2022-10-28 19:53:37 +0200

    Show first page in a book on load

--- src/cljs/webchange/lesson_builder/layout/stage/views.cljs
+++ src/cljs/webchange/lesson_builder/layout/stage/views.cljs
@@ -5,6 +5,7 @@
     [webchange.interpreter.renderer.stage :refer [stage]]
     [webchange.lesson-builder.layout.stage.second-stage.views :refer [second-stage]]
     [webchange.lesson-builder.layout.stage.state :as state]
+    [webchange.lesson-builder.state-flipbook :as flipbook-state]
     [webchange.lesson-builder.widgets.page-remove.views :refer [remove-flipbook-page]]
     [webchange.lesson-builder.widgets.select-stage.views :refer [select-stage]]
     [webchange.ui.index :as ui]))
@@ -15,7 +16,10 @@
         key @(re-frame/subscribe [::state/stage-key])
         show-flipbook-actions? @(re-frame/subscribe [::state/show-flipbook-actions?])
         busy? @(re-frame/subscribe [::state/stage-busy?])
-        handle-ready #(re-frame/dispatch [::state/set-stage-ready true])
+        handle-ready (fn []
+                       (re-frame/dispatch [::state/set-stage-ready true])
+                       (when show-flipbook-actions?
+                         (re-frame/dispatch [::flipbook-state/show-flipbook-stage 0])))
         current-page @(re-frame/subscribe [::state/current-page-side])]
     [:div {:id         "block--stage"
            :class-name class-name}
