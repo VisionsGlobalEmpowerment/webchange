81e5359ee96707679f5b1d64ba6b25152c5b0a95
Ivan Khaldeev <ikhaldeev@yandex.ru>
2022-11-03 16:04:02 +0100

    Change verify account email subject

--- src/clj/webchange/emails/templates/core.clj
+++ src/clj/webchange/emails/templates/core.clj
@@ -4,7 +4,7 @@
     [webchange.emails.templates.reset-password :refer [reset-password-template]]))
 
 (def templates
-  {:email-confirmation {:subject "Blue Bricks Account Activation"
+  {:email-confirmation {:subject "Blue Brick School Verify Account"
                         :template email-confirmation-template}
    :reset-password {:subject "Blue Bricks Account Password Reset"
                     :template reset-password-template}})
