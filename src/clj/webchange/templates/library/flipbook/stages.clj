aa4c9f15c27c38a467cdae3f9169958ab61909ec
Ivan Khaldeev <ikhaldeev@yandex.ru>
2022-11-09 16:12:04 +0100

    Spread names

--- src/clj/webchange/templates/library/flipbook/stages.clj
+++ src/clj/webchange/templates/library/flipbook/stages.clj
@@ -16,18 +16,8 @@
     ""))
 
 (defn- get-stage-title
-  [{:keys [activity left-page right-page stage-idx]}]
-  (let [not-empty? (complement empty?)
-        left-page-title (get-page-title activity left-page)
-        right-page-title (get-page-title activity right-page)]
-    (if (and (empty? left-page-title)
-             (empty? right-page-title))
-      (str "Spread " stage-idx)
-      (cond-> ""
-              (not-empty? left-page-title) (str left-page-title)
-              (and (not-empty? left-page-title)
-                   (not-empty? right-page-title)) (str " | ")
-              (not-empty? right-page-title) (str right-page-title)))))
+  [{:keys [stage-idx]}]
+  (str "Spread " stage-idx))
 
 (defn- page-position->page-idx
   [page-position pages]
@@ -45,13 +35,8 @@
         stages-number (-> (count pages) (quot 2) (inc))
         stages (map (fn [stage-index]
                       (let [left-page-index (-> (* stage-index 2) (dec) (page-position->page-idx pages))
-                            left-page (index->page left-page-index pages)
                             right-page-index (-> (* stage-index 2) (page-position->page-idx pages))
-                            right-page (index->page right-page-index pages)
-                            stage-title (get-stage-title {:left-page  left-page
-                                                          :right-page right-page
-                                                          :activity   activity-data
-                                                          :stage-idx  stage-index})]
+                            stage-title (get-stage-title {:stage-idx  stage-index})]
                         {:name      stage-title
                          :idx       stage-index
                          :pages-idx [left-page-index right-page-index]}))
